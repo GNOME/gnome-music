78c7f168c90d6836c02a33a54fdacc2674dd692b
Seif Lotfy <seif@lotfy.com>
2013-05-06 18:30:27 +0200

    Add "Matteo Settenvini" to the copyrights of albumArtCache.js due to his large refactoring contribution

--- src/albumArtCache.js
+++ src/albumArtCache.js
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2013 Seif Lotfy <seif@lotfy.com>.
  * Copyright (c) 2013 Vadim Rutkovsky <vrutkovs@redhat.com>.
+ * Copyright (c) 2013 Matteo Settenvini <matteo@member.fsf.org>
  *
  * Gnome Music is free software; you can Public License as published by the
  * Free Software Foundation; either version 2 of the License, or (at your
