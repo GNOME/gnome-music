20d6e1aefd5666fe7c3e5fe5b8da5364dfa541d4
Vadim Rutkovsky <vrutkovs@redhat.com>
2013-05-07 15:32:37 +0200

    tests: as we're loading just one song, next button will also be disabled

--- tests/tests_albumPlayback.js
+++ tests/tests_albumPlayback.js
@@ -129,7 +129,8 @@ function testAlbumViewPlayback() {
     // Buttons become enabled
     assertFalse(player.prevBtn.get_sensitive())
     assertTrue(player.playBtn.get_sensitive())
-    assertTrue(player.nextBtn.get_sensitive())
+    // This is set to False just because tests wait for one and only track to be loaded
+    assertFalse(player.nextBtn.get_sensitive())
 
     // Scale value is set to 0
     assertEquals(player.progressScale.get_value(), 0)
