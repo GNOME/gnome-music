c02dcb9125af4ef00d1862dacce9fc71657f85b5
Sai <suman.sai14@gmail.com>
2013-07-12 18:10:24 +0530

    inital commit

--- src/application.py
+++ src/application.py
@@ -2,11 +2,11 @@ from gi.repository import Gtk, Gio, GLib
 
 class Application(gtk.Application):
     def __init__(self):
-        super().__init__(
+        super().__init__({
             application_id: 'org.gnome.Music',
             flags: gio.ApplicationFlags.FLAGS_NONE,
-            inactivity_timeout: 12000
+            inactivity_timeout: 12000}
         )
-        Glib.set_application_name("Music")
+        GLib.set_application_name("Music")
 
 
