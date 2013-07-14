7d6ba5326c346bd9e8376266e397bb2533b7fa78
Guillaume Quintard <guillaume.quintard@gmail.com>
2013-07-14 13:30:01 +0200

    add gnome-music.in

--- gnome-music/__init__.py
+++ gnome-music/__init__.py
@@ -1,6 +1,6 @@
-from application import Application
+from gnomemusic import application
 import sys
 
 if __name__ == 'gnome-music':
-    app = Application()
+    app = Application.application()
     sys.exit(app.run(sys.argv))
