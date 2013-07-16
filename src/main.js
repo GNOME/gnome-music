35cb714df715dcabad03ab16926ac57e98fe5f2b
Vadim Rutkovsky <vrutkovs@redhat.com>
2013-07-16 12:12:04 +0200

    Add COPYING file
    Update source file license and copyrights (fixed typo)
    
    https://bugzilla.gnome.org/show_bug.cgi?id=700776

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 /*
- * Copyright (c) 2013 Eslam Mostafa <cseslam@gmail.com>.
- * Copyright (c) 2013 Seif Lotfy <seif@lotfy.com>.
+ * Copyright (c) 2013 Eslam Mostafa <cseslam@gmail.com>
+ * Copyright (c) 2013 Seif Lotfy <seif@lotfy.com>
+ * Copyright (c) 2013 Giovanni Campagna
  *
- * Gnome Music is free software; you can Public License as published by the
+ * Gnome Music is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
  * Free Software Foundation; either version 2 of the License, or (at your
  * option) any later version.
+ *
+ * The Gnome Music authors hereby grant permission for non-GPL compatible
+ * GStreamer plugins to be used and distributed together with GStreamer and
+ * Gnome Music. This permission is above and beyond the permissions granted by
+ * the GPL license by which Gnome Music is covered. If you modify this code, you may 
+ * extend this exception to your version of the code, but you are not obligated 
+ * to do so. If you do not wish to do so, delete this exception statement from 
+ * your version.
  *
  * Gnome Music is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
--- src/query.js
+++ src/query.js
@@ -1,11 +1,21 @@
 /*
 * Copyright (c) 2013 Igalia S.L.
 * Authored by: Juan A. Suarez Romero <jasuarez@igalia.com>
-* Copyright (c) 2013 Vadim Rutkovsky <vrutkovs@redhat.com>.
+* Copyright (c) 2013 Vadim Rutkovsky <vrutkovs@redhat.com>
+* Copyright (c) 2013 Sai Suman Prayaga
+* Copyright (c) 2013 Seif Lotfy <seif@lotfy.com>
 *
 * Gnome Music is free software; you can Public License as published by the
 * Free Software Foundation; either version 2 of the License, or (at your
 * option) any later version.
+*
+* The Gnome Music authors hereby grant permission for non-GPL compatible
+* GStreamer plugins to be used and distributed together with GStreamer and
+* Gnome Music. This permission is above and beyond the permissions granted by
+* the GPL license by which Gnome Music is covered. If you modify this code, you may 
+* extend this exception to your version of the code, but you are not obligated 
+* to do so. If you do not wish to do so, delete this exception statement from 
+* your version.
 *
 * Gnome Music is distributed in the hope that it will be useful, but
 * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
