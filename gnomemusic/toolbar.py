08c213be172bd948ca1c357a9efe0f6a053d2d06
Vadim Rutkovsky <vrutkovs@redhat.com>
2014-04-15 13:54:02 +0200

    pep8: remove extra Gdk import in toolbar

--- gnomemusic/toolbar.py
+++ gnomemusic/toolbar.py
@@ -28,7 +28,7 @@
 # delete this exception statement from your version.
 
 
-from gi.repository import Gtk, Gdk, GObject
+from gi.repository import Gtk, GObject
 
 from gnomemusic.searchbar import Searchbar
 
