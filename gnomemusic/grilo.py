b72a5df07feaab61b6b9507c73498333cab891eb
Marinus Schraal <mschraal@src.gnome.org>
2016-10-11 14:12:37 +0200

    query: Add spotify cover to Grilo rank

--- gnomemusic/grilo.py
+++ gnomemusic/grilo.py
@@ -32,7 +32,11 @@ from gnomemusic.query import Query
 from gnomemusic import log, TrackerWrapper
 import logging
 import os
-os.environ['GRL_PLUGIN_RANKS'] = 'grl-local-metadata:4,grl-filesystem:3,grl-tracker-source:2,grl-lastfm-cover:1'
+os.environ['GRL_PLUGIN_RANKS'] = ("grl-local-metadata:5,"
+                                  "grl-filesystem:4,"
+                                  "grl-tracker-source:3,"
+                                  "grl-spotify-cover:2,"
+                                  "grl-lastfm-cover:1")
 from gi.repository import Grl
 logger = logging.getLogger(__name__)
 
