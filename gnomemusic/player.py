27db20988296969e9775f89fe3fcc9c6594a53ee
Felipe Borges <felipeborges@gnome.org>
2018-01-28 20:36:37 +0100

    player: Introduce the Playback Popover
    This widget allows the user to browse through the app without
    losing track of the current playlist (what's playing next).
    
    It is based this wireframes
    https://raw.githubusercontent.com/gnome-design-team/gnome-mockups/
    master/music/wire-playback-buffer.png
    
    This feature has been worked on Outreachy by Jordana Luft, but
    that branch differs entirely from the current state of the code
    base. Its been two years.
    
    Fixes #14

--- gnomemusic/player.py
+++ gnomemusic/player.py
@@ -50,6 +50,7 @@ from gnomemusic.albumartcache import AlbumArtCache, DefaultIcon, ArtSize
 from gnomemusic.grilo import grilo
 from gnomemusic.playlists import Playlists
 from gnomemusic.scrobbler import LastFmScrobbler
+from gnomemusic.widgets.playbackpopover import PlaybackPopover
 import gnomemusic.utils as utils
 
 
@@ -775,6 +776,8 @@ class Player(GObject.GObject):
         self.nextBtn = self._ui.get_object('next_button')
         self._playImage = self._ui.get_object('play_image')
         self._pauseImage = self._ui.get_object('pause_image')
+        self._nowplayingButton = self._ui.get_object('nowplaying_button')
+        self._playbackPopover = PlaybackPopover(self._nowplayingButton, self)
         self.progressScale = self._ui.get_object('progress_scale')
         self.songPlaybackTimeLabel = self._ui.get_object('playback')
         self.songTotalTimeLabel = self._ui.get_object('duration')
@@ -792,6 +795,7 @@ class Player(GObject.GObject):
         self.prevBtn.connect('clicked', self._on_prev_btn_clicked)
         self.playBtn.connect('clicked', self._on_play_btn_clicked)
         self.nextBtn.connect('clicked', self._on_next_btn_clicked)
+        self._playbackPopover.connect('current-changed', self._on_playback_popover_current_changed)
         self.progressScale.connect('button-press-event', self._on_progress_scale_event)
         self.progressScale.connect('value-changed', self._on_progress_value_changed)
         self.progressScale.connect('button-release-event', self._on_progress_scale_button_released)
@@ -801,6 +805,10 @@ class Player(GObject.GObject):
         self._seek_timeout = None
         self._old_progress_scale_value = 0.0
         self.progressScale.set_increments(300, 600)
+
+    def _on_playback_popover_current_changed(self, popover, iter):
+        self.currentTrack = Gtk.TreeRowReference.new(self.playlist, self.playlist.get_path(iter))
+        self.play()
 
     def _on_progress_scale_seek_finish(self, value):
         """Prevent stutters when seeking with infinitesimal amounts"""
