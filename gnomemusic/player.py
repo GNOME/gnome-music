af2d42f214bcbfe3a8d58b1d418d4da73bf2d90b
Marinus Schraal <mschraal@gnome.org>
2018-02-16 13:53:40 +0100

    gstplayer: Remove is_playing function
    Use state instead.

--- gnomemusic/player.py
+++ gnomemusic/player.py
@@ -355,7 +355,7 @@ class Player(GObject.GObject):
 
     @property
     def playing(self):
-        return self._player.is_playing()
+        return self._player.state == Playback.PLAYING
 
     @log
     def _on_state_change(self, klass, arguments):
@@ -363,7 +363,7 @@ class Player(GObject.GObject):
 
     @log
     def _sync_playing(self):
-        if self._player.is_playing():
+        if self._player.state == Playback.PLAYING:
             image = self._pause_image
             tooltip = _("Pause")
         else:
@@ -777,7 +777,7 @@ class Player(GObject.GObject):
 
     @log
     def _on_play_button_clicked(self, button):
-        if self._player.is_playing():
+        if self._player.state == Playback.PLAYING:
             self.pause()
         else:
             self.play()
