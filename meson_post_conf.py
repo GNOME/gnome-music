aca5adeb2e3023792ab79ef194b00101564bfc98
Marinus Schraal <mschraal@gnome.org>
2018-02-22 13:05:44 +0100

    meson: Make music run in the builddir
    This is a bit of hacky mess, probably want to rethink this.

---
+++ meson_post_conf.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+
+build_root = os.environ.get('MESON_BUILD_ROOT')
+source_root = os.environ.get('MESON_SOURCE_ROOT')
+
+subprocess.call(['glib-compile-schemas', source_root + '/data/'])
+subprocess.call(['mkdir', '-p', build_root + '/data/glib-2.0/schemas'])
+subprocess.call(['mv', source_root + '/data/gschemas.compiled', build_root + '/data/glib-2.0/schemas'])
