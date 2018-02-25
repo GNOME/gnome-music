91ecc820059c0d41358bc76d3eeb0567298646ea
Marinus Schraal <mschraal@gnome.org>
2018-02-25 10:36:07 +0100

    Revert "player: Dedupe play/pause functionality"
    This reverts commit f352ca1d038049070c7a658dceffa9707220c8fd.

--- gnomemusic/player.py
+++ gnomemusic/player.py
@@ -560,7 +560,10 @@ class Player(GObject.GObject):
 
     @log
     def play_pause(self):
-        self._sync_playing()
+        if self._player.state == Playback.PLAYING:
+            self.set_playing(False)
+        else:
+            self.set_playing(True)
 
     @log
     def _create_model(self, model, model_iter):
