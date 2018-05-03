e4febd6e6a26f7f8845a7edb40b2d5778799c6f7
Jean Felder <jfelder@src.gnome.org>
2018-05-03 22:14:15 +0200

    views: Merge empty views and InitialStateView
    EmptyView, EmptySearchView and InitialStateView are almost
    identical. The only differences are the displayed icon and two labels.
    Create one EmptyView class which handles the three cases.
    
    Update Window class to switch more easily between empty and player
    states.

--- gnomemusic/views/emptyview.py
+++ gnomemusic/views/emptyview.py
@@ -22,31 +22,101 @@
 # code, but you are not obligated to do so.  If you do not wish to do so,
 # delete this exception statement from your version.
 
+from enum import IntEnum
+
 from gettext import gettext as _
-from gi.repository import Gtk
+from gi.repository import GObject, Gtk
 
 from gnomemusic import log
+from gnomemusic.albumartcache import Art
 from gnomemusic.query import Query
 
 
+@Gtk.Template(resource_path="/org/gnome/Music/EmptyView.ui")
 class EmptyView(Gtk.Stack):
+    """Empty view when there is no music to display
+
+    This view can have three states
+    INITIAL means that Music app has never been initialized and no music
+    has been found
+    EMPTY means that no music has been found at startup
+    SEARCH is the empty search view: no music found during a search
+    """
+
+    class State(IntEnum):
+        """Enum for EmptyView state."""
+        INITIAL = 0
+        EMPTY = 1
+        SEARCH = 2
+
+    __gtype_name__ = "EmptyView"
+
+    _information_label = Gtk.Template.Child()
+    _main_label = Gtk.Template.Child()
+    _icon = Gtk.Template.Child()
 
     def __repr__(self):
-        return '<EmptyView>'
+        return "<EmptyView>"
 
     @log
-    def __init__(self, window, player):
-        super().__init__(transition_type=Gtk.StackTransitionType.CROSSFADE)
+    def __init__(self):
+        super().__init__()
 
-        self.builder = Gtk.Builder()
-        self.builder.add_from_resource('/org/gnome/Music/NoMusic.ui')
-        widget = self.builder.get_object('container')
-        self.update_empty_state_link()
-        self.add(widget)
+        href_text = "<a href='{}'>{}</a>".format(
+            Query.MUSIC_URI, _("Music folder"))
+
+        # TRANSLATORS: This is a label to display a link to open user's music
+        # folder. {} will be replaced with the translated text 'Music folder'
+        folder_text = _("The contents of your {} will appear here.")
+        self._content_text = folder_text.format(href_text)
+
+        self._state = EmptyView.State.INITIAL
+
+    @GObject.Property(type=int, default=0, minimum=0, maximum=2)
+    @log
+    def state(self):
+        """Get the state of the empty view
+
+        :returns: The view state
+        :rtype: int
+        """
+        return self._state
+
+    @state.setter
+    @log
+    def state(self, value):
+        """Set the state of the empty view
+
+        :param int value: new state
+        """
+        self._state = value
+        if self._state == EmptyView.State.INITIAL:
+            self._set_initial_state()
+        elif self._state == EmptyView.State.EMPTY:
+            self._set_empty_state()
+        elif self._state == EmptyView.State.SEARCH:
+            self._set_search_state()
         self.show_all()
 
-    def update_empty_state_link(self):
-        label = self.builder.get_object('empty-state-label')
-        href_text = '<a href="%s">%s</a>' % (Query.MUSIC_URI,
-                                             _("Music folder"))
-        label.set_label(label.get_label() % href_text)
+    @log
+    def _set_initial_state(self):
+        self._information_label.props.label = self._content_text
+        self._main_label.props.label = _("Hey DJ")
+        self._main_label.props.margin_bottom = 18
+
+        self._icon.props.resource = "/org/gnome/Music/initial-state.png"
+        self._icon.props.margin_bottom = 32
+        self._icon.props.height_request = Art.Size.LARGE.height
+        self._icon.props.width_request = Art.Size.LARGE.width
+
+    @log
+    def _set_empty_state(self):
+        self._main_label.props.label = _("No music found")
+        self._information_label.props.label = self._content_text
+
+    @log
+    def _set_search_state(self):
+        self._main_label.props.margin_bottom = 12
+        self._main_label.props.label = _("No music found")
+        self._icon.props.margin_bottom = 18
+        self._information_label.props.label = _("Try a different search")
