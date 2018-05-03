ca5dc992d8f9c832f2a1b4dc41e67fedc99d9cb9
Jean Felder <jfelder@src.gnome.org>
2018-05-03 13:47:42 +0200

    artistsview: Port sidebar to GtkListBox
    It removes all libgd dependencies.
    
    Closes: #18

--- gnomemusic/views/artistsview.py
+++ gnomemusic/views/artistsview.py
@@ -24,7 +24,7 @@
 
 import logging
 from gettext import gettext as _
-from gi.repository import Gd, GLib, Gtk, Pango
+from gi.repository import Gdk, GObject, GLib, Gtk, Pango
 
 from gnomemusic import log
 from gnomemusic.grilo import grilo
@@ -52,99 +52,76 @@ class ArtistsView(BaseView):
         :param GtkWidget window: The main window
         :param player: The main player object
         """
+        self._sidebar = Gtk.ListBox()
+        sidebar_container = Gtk.ScrolledWindow()
+        sidebar_container.add(self._sidebar)
+
         super().__init__(
-            'artists', _("Artists"), window, Gd.MainViewType.LIST, True)
+            'artists', _("Artists"), window, None, True, sidebar_container)
 
         self.player = player
         self._artists = {}
-        self._albums_selected = []
-        self._items_selected = []
-        self._items_selected_callback = None
-        self._artist_albums_stack = Gtk.Stack(
-            transition_type=Gtk.StackTransitionType.CROSSFADE)
-        self._artist_albums_widget = Gtk.Frame(shadow_type=Gtk.ShadowType.NONE,
-                                               hexpand=True)
-        self._artist_albums_stack.add_named(self._artist_albums_widget,
-                                            "sidebar")
-        self._artist_albums_stack.set_visible_child_name("sidebar")
-        self._view.set_shadow_type(Gtk.ShadowType.IN)
-        self._view.get_style_context().add_class('side-panel')
-        self._view.set_hexpand(False)
-        self._view.get_generic_view().get_selection().set_mode(
-            Gtk.SelectionMode.SINGLE)
-        self._grid.attach(self._artist_albums_stack, 2, 0, 2, 2)
-        self._add_list_renderers()
-        self._view.get_generic_view().get_style_context().remove_class(
-            'content-view')
+
+        sidebar_container.props.width_request = 220
+        sidebar_container.get_style_context().add_class('side-panel')
+        self._sidebar.get_style_context().add_class('view')
+        self._sidebar.props.selection_mode = Gtk.SelectionMode.SINGLE
+        self._sidebar.connect('row-activated', self._on_artist_activated)
+        self._sidebar.connect('button-release-event', self._on_sidebar_clicked)
+
         self.show_all()
-        self._view.hide()
+        self._sidebar.hide()
+
+    @log
+    def _setup_view(self, view_type):
+        view_container = Gtk.ScrolledWindow(hexpand=True, vexpand=True)
+        self._box.pack_start(view_container, True, True, 0)
+
+        self._view = Gtk.Stack(
+            transition_type=Gtk.StackTransitionType.CROSSFADE)
+        view_container.add(self._view)
+
+        self._artist_albums_widget = Gtk.Frame(
+            shadow_type=Gtk.ShadowType.NONE, hexpand=True)
+        self._view.add_named(self._artist_albums_widget, "artist-albums")
+        self._view.props.visible_child_name = "artist-albums"
 
     @log
     def _on_changes_pending(self, data=None):
         if (self._init
                 and not self._header_bar.selection_mode):
-            self.model.clear()
             self._artists.clear()
             self._offset = 0
             GLib.idle_add(self._populate)
             grilo.changes_pending['Artists'] = False
 
     @log
-    def _set_selection(self, value, parent=None):
-        count = 0
-        _iter = self.model.iter_children(parent)
-        while _iter is not None:
-            if self.model.iter_has_child(_iter):
-                count += self._set_selection(value, _iter)
-            self.model[_iter][6] = value
-            count += 1
-            _iter = self.model.iter_next(_iter)
-        return count
-
-    @log
-    def _add_list_renderers(self):
-        list_widget = self._view.get_generic_view()
-        cols = list_widget.get_columns()
-        cells = cols[0].get_cells()
-        cells[1].set_visible(False)
-        cells[2].set_visible(False)
-        self.text_renderer = Gd.StyledTextRenderer(
-            xpad=16, ypad=16, ellipsize=Pango.EllipsizeMode.END, xalign=0.0,
-            width=220)
-        list_widget.add_renderer(self.text_renderer, lambda *args: None, None)
-        cols[0].clear_attributes(self.text_renderer)
-        cols[0].add_attribute(self.text_renderer, 'text', 2)
-
-    @log
-    def _on_item_activated(self, widget, item_id, path):
+    def _on_artist_activated(self, sidebar, row, data=None):
         """Initializes new artist album widgets"""
-        try:
-            itr = self.model.get_iter(path)
-        except ValueError as err:
-            logger.warning("Error: {}, {}".format(err.__class__, err))
+        if self.selection_mode:
+            row.check.props.active = not row.check.props.active
             return
 
-        self._last_selection = itr
-        artist = self.model[itr][2]
+        self._last_selected_row = row
+        artist = row.artist
         albums = self._artists[artist.casefold()]['albums']
         widget = self._artists[artist.casefold()]['widget']
 
         if widget:
-            artist_stack = self._artist_albums_stack
             if self.player.running_playlist('Artist', widget.artist):
                 self._artist_albums_widget = widget.get_parent()
-                GLib.idle_add(self._artist_albums_stack.set_visible_child,
-                              self._artist_albums_widget)
+                GLib.idle_add(
+                    self._view.set_visible_child, self._artist_albums_widget)
                 return
-            elif widget.get_parent() == artist_stack:
+            elif widget.get_parent() == self._view:
                 return
             else:
                 widget.get_parent().destroy()
 
         # Prepare a new artist_albums_widget here
-        new_artist_albums_widget = Gtk.Frame(shadow_type=Gtk.ShadowType.NONE,
-                                             hexpand=True)
-        self._artist_albums_stack.add(new_artist_albums_widget)
+        new_artist_albums_widget = Gtk.Frame(
+            shadow_type=Gtk.ShadowType.NONE, hexpand=True)
+        self._view.add(new_artist_albums_widget)
 
         artist_albums = ArtistAlbumsWidget(
             artist, albums, self.player, self, self._header_bar,
@@ -155,22 +132,37 @@ class ArtistsView(BaseView):
 
         # Replace previous widget
         self._artist_albums_widget = new_artist_albums_widget
-        GLib.idle_add(self._artist_albums_stack.set_visible_child,
-                      new_artist_albums_widget)
+        GLib.idle_add(self._view.set_visible_child, new_artist_albums_widget)
 
     @log
     def _add_item(self, source, param, item, remaining=0, data=None):
         if (not item and remaining == 0):
-            self._view.set_model(self.model)
             self._window.notifications_popup.pop_loading()
-            self._view.show()
+            self._sidebar.show()
             return
         self._offset += 1
         artist = utils.get_artist_name(item)
         if not artist.casefold() in self._artists:
-            itr = self.model.insert_with_valuesv(-1, [2], [artist])
+            # populate sidebar
+            row = Gtk.ListBoxRow()
+            row.artist = artist
+            box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+            row.add(box)
+            row.check = Gtk.CheckButton()
+            row.check.connect('toggled', self._on_selection_toggled)
+            artist_label = Gtk.Label(
+                label=artist, xalign=0, xpad=16, ypad=16,
+                ellipsize=Pango.EllipsizeMode.END)
+            box.pack_start(row.check, False, True, 0)
+            box.pack_start(artist_label, True, True, 0)
+            self._sidebar.add(row)
+            row.show_all()
+            row.check.hide()
+            row.check.bind_property(
+                'visible', self, 'selection_mode',
+                GObject.BindingFlags.BIDIRECTIONAL)
+
             self._artists[artist.casefold()] = {
-                'iter': itr,
                 'albums': [],
                 'widget': None
             }
@@ -183,27 +175,51 @@ class ArtistsView(BaseView):
         grilo.populate_artists(self._offset, self._add_item)
 
     @log
-    def _on_header_bar_toggled(self, button):
-        BaseView._on_header_bar_toggled(self, button)
+    def _on_sidebar_clicked(self, widget, event):
+        if event.button != Gdk.BUTTON_SECONDARY:
+            return
 
-        view_selection = self._view.get_generic_view().get_selection()
-        if button.get_active():
-            self.text_renderer.set_fixed_size(178, -1)
-            self._last_selection = view_selection.get_selected()[1]
-            view_selection.set_mode(Gtk.SelectionMode.NONE)
-        else:
-            self.text_renderer.set_fixed_size(220, -1)
-            view_selection.set_mode(Gtk.SelectionMode.SINGLE)
-            if self._last_selection is not None:
-                view_selection.select_iter(self._last_selection)
+        if not self.selection_mode:
+            self._on_selection_mode_request()
+
+        row = self._sidebar.get_row_at_y(event.y)
+        row.check.props.active = not row.check.props.active
+
+    @log
+    def _on_selection_toggled(self, widget, data=None):
+        selected_artists = 0
+        for row in self._sidebar:
+            if row.check.props.active:
+                selected_artists += 1
+
+        self.update_header_from_selection(selected_artists)
 
     @log
     def _on_selection_mode_changed(self, widget, data=None):
-        self._artist_albums_stack.set_sensitive(
-            not self._header_bar.selection_mode)
+        self._view.props.sensitive = not self._header_bar.selection_mode
+        if self.selection_mode:
+            self._sidebar.props.selection_mode = Gtk.SelectionMode.NONE
+        else:
+            self._sidebar.props.selection_mode = Gtk.SelectionMode.SINGLE
+
         if (not self._header_bar.selection_mode
                 and grilo.changes_pending['Artists']):
             self._on_changes_pending()
+
+    @log
+    def _toggle_all_selection(self, selected):
+        for row in self._sidebar:
+            row.check.props.active = selected
+
+    @log
+    def select_all(self):
+        self._toggle_all_selection(True)
+        self.update_header_from_selection(len(self._sidebar))
+
+    @log
+    def unselect_all(self):
+        self._toggle_all_selection(False)
+        self.update_header_from_selection(0)
 
     @log
     def get_selected_songs(self, callback):
@@ -213,32 +229,30 @@ class ArtistsView(BaseView):
         :returns: All selected songs
         :rtype: A list of songs
         """
-        self._items_selected = []
-        self._items_selected_callback = callback
-        self._albums_index = 0
-        self._albums_selected = []
+        selected_albums = []
+        for row in self._sidebar:
+            if row.check.props.active:
+                artist = row.artist
+                albums = self._artists[artist.casefold()]['albums']
+                selected_albums.extend(albums)
 
-        for path in self._view.get_selection():
-            itr = self.model.get_iter(path)
-            artist = self.model[itr][2]
-            albums = self._artists[artist.casefold()]['albums']
-            self._albums_selected.extend(albums)
-
-        if len(self._albums_selected):
-            self._get_selected_album_songs()
+        if len(selected_albums) > 0:
+            self._get_selected_albums_songs(selected_albums, callback)
 
     @log
-    def _get_selected_album_songs(self):
-        grilo.populate_album_songs(self._albums_selected[self._albums_index],
-                                   self._add_selected_item)
-        self._albums_index += 1
+    def _get_selected_albums_songs(self, albums, callback):
+        selected_songs = []
+        self._album_index = 0
 
-    @log
-    def _add_selected_item(self, source, param, item, remaining=0, data=None):
-        if item:
-            self._items_selected.append(item)
-        if remaining == 0:
-            if self._albums_index < len(self._albums_selected):
-                self._get_selected_album_songs()
-            else:
-                self._items_selected_callback(self._items_selected)
+        def add_songs(source, param, item, remaining, data=None):
+            if item:
+                selected_songs.append(item)
+            if remaining == 0:
+                self._album_index += 1
+                if self._album_index < len(albums):
+                    grilo.populate_album_songs(
+                        albums[self._album_index], add_songs)
+                else:
+                    callback(selected_songs)
+
+        grilo.populate_album_songs(albums[self._album_index], add_songs)
