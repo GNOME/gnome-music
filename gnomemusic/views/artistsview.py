GNOME/gnome-music

commit 85066a19148e6806c0036ba3eedbdee2d42a9036

artistalbumswidget: Make artist variable private

gnomemusic/widgets/artistalbumswidget.py
@@ -44,7 +44,7 @@ class ArtistAlbumsWidget(Gtk.Box):
 
     __gtype_name__ = 'ArtistAlbumsWidget'
 
-    _artist = Gtk.Template.Child()
+    _artist_label = Gtk.Template.Child()
 
     def __repr__(self):
         return '<ArtistAlbumsWidget>'
@@ -54,7 +54,7 @@ class ArtistAlbumsWidget(Gtk.Box):
                  selection_toolbar, window, selection_mode_allowed=False):
         super().__init__(orientation=Gtk.Orientation.VERTICAL)
         self._player = player
-        self.artist = artist
+        self._artist = artist
         self._window = window
         self._parent_view = parent_view
         self._selection_mode = False
@@ -62,7 +62,7 @@ class ArtistAlbumsWidget(Gtk.Box):
         self._selection_toolbar = selection_toolbar
         self._header_bar = header_bar
 
-        self._artist.props.label = self.artist
+        self._artist_label.props.label = self._artist
 
         self._widgets = []
 
@@ -135,7 +135,7 @@ class ArtistAlbumsWidget(Gtk.Box):
 
     @log
     def _update_model(self, player, playlist, current_iter):
-        if not player.running_playlist('Artist', self.artist):
+        if not player.running_playlist('Artist', self._artist):
             self._clean_model()
             return False
 

data/ArtistAlbumsWidget.ui
@@ -9,7 +9,7 @@
     <property name="margin_top">18</property>
     <property name="orientation">vertical</property>
     <child>
-      <object class="GtkLabel" id="_artist">
+      <object class="GtkLabel" id="_artist_label">
         <property name="visible">True</property>
         <property name="can_focus">False</property>
         <property name="xalign">0</property>

commit f45da5e9a656391261f66c4d40ae9acebd0f8aee

artistalbumswidget: Create artist GObject property
artist becomes a read-only GObject property corresponding to the
artist name.
This fixes a crash caused by commit 85066a19 (make artist variable
private).

gnomemusic/views/artistsview.py
@@ -131,7 +131,7 @@ class ArtistsView(BaseView):
 
         if widget:
             artist_stack = self._artist_albums_stack
-            if self.player.playing_playlist('Artist', widget.artist):
+            if self.player.playing_playlist('Artist', widget.props.artist):
                 self._artist_albums_widget = widget.get_parent()
                 GLib.idle_add(self._artist_albums_stack.set_visible_child,
                               self._artist_albums_widget)

gnomemusic/widgets/artistalbumswidget.py
@@ -57,13 +57,12 @@ class ArtistAlbumsWidget(Gtk.Box):
             window, selection_mode_allowed=False):
         super().__init__(orientation=Gtk.Orientation.VERTICAL)
         self._player = player
-        self._artist = artist
         self._window = window
         self._selection_mode_allowed = selection_mode_allowed
         self._selection_toolbar = selection_toolbar
         self._header_bar = header_bar
 
-        self._artist_label.props.label = self._artist
+        self._artist_label.props.label = artist
 
         self._widgets = []
 
@@ -141,7 +140,7 @@ class ArtistAlbumsWidget(Gtk.Box):
 
     @log
     def _update_model(self, player, playlist, current_iter):
-        if not player.playing_playlist('Artist', self._artist):
+        if not player.playing_playlist('Artist', self.props.artist):
             self._clean_model()
             return False
 
@@ -203,3 +202,8 @@ class ArtistAlbumsWidget(Gtk.Box):
         """Deselect all items"""
         for widget in self._widgets:
             widget.select_none()
+
+    @GObject.Property(type=str, flags=GObject.ParamFlags.READABLE)
+    def artist(self):
+        """Artist name"""
+        return self._artist_label.props.label