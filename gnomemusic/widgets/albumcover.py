f42fecede9fac4b139b035cee1bff6fcbffde7bd
Marinus Schraal <mschraal@gnome.org>
2018-08-02 09:27:10 +0200

    albumcover: Move event handling to internal

--- gnomemusic/widgets/albumcover.py
+++ gnomemusic/widgets/albumcover.py
@@ -61,6 +61,20 @@ class AlbumCover(Gtk.FlowBoxChild):
             'selection-mode', self._check, 'visible',
             GObject.BindingFlags.BIDIRECTIONAL)
 
+        self._events.add_events(Gdk.EventMask.TOUCH_MASK)
+        self._events.connect(
+            'button-release-event', self._on_album_event_triggered)
+
     @GObject.Property(type=Grl.Media, flags=GObject.ParamFlags.READABLE)
     def media(self):
         return self._media
+
+    @log
+    def _on_album_event_triggered(self, evbox, event, data=None):
+        modifiers = Gtk.accelerator_get_default_mod_mask()
+        if ((event.state & modifiers) == Gdk.ModifierType.CONTROL_MASK
+                and not self.props.selection_mode):
+            self.props.selection_mode = True
+
+        if self.props.selection_mode:
+            self._check.props.active = not self._check.props.active
