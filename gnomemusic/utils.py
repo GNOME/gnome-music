1ab750fd1a153c5965343529126341671cce3af5
Jean Felder <jfelder@src.gnome.org>
2018-09-03 08:11:02 +0200

    utils: Map grilo tags and utils functions
    Add a dictionnary to map a tag name with the corresponding utils
    function to get it.

--- gnomemusic/utils.py
+++ gnomemusic/utils.py
@@ -164,3 +164,15 @@ def seconds_to_string(duration):
     seconds %= 60
 
     return '{:d}:{:02d}'.format(minutes, seconds)
+
+
+fields = {
+    'album': get_album_title,
+    'artist': get_artist_name,
+    'composer': get_composer_name,
+    'disc': get_album_disc_nr,
+    'genre': get_media_genre,
+    'title': get_media_title,
+    'track': get_media_track_nr,
+    'year': get_media_year
+    }
