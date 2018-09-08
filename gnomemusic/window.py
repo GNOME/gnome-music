1fd6559551e25c3fbedc64766b2867a77cfa26b7
Marinus Schraal <mschraal@gnome.org>
2018-09-08 14:41:18 +0200

    playertoolbar: Shift visibility logic around
    Move most of the PlayerToolbar visibility logic to Window, however on
    startup the toolbar is not shown. For that special case there is some
    additional logic in Window and PlayerToolbar itself.
    This also removes one init argument from PlayerToolbar.

--- gnomemusic/window.py
+++ gnomemusic/window.py
@@ -130,7 +130,7 @@ class Window(Gtk.ApplicationWindow):
     def _setup_view(self):
         self._headerbar = HeaderBar()
 
-        self._player_toolbar = PlayerToolbar(self._player, self)
+        self._player_toolbar = PlayerToolbar(self._player)
         self.views = [None] * len(View)
 
         self._headerbar.bind_property(
@@ -152,8 +152,10 @@ class Window(Gtk.ApplicationWindow):
             GObject.BindingFlags.BIDIRECTIONAL |
             GObject.BindingFlags.SYNC_CREATE)
         self.bind_property(
-            'selection-mode', self._selection_toolbar, 'visible',
-            GObject.BindingFlags.SYNC_CREATE)
+            'selection-mode', self._player_toolbar, 'visible',
+            GObject.BindingFlags.INVERT_BOOLEAN)
+        self.bind_property(
+            'selection-mode', self._selection_toolbar, 'visible')
         # Create only the empty view at startup
         # if no music, switch to empty view and hide stack
         # if some music is available, populate stack with mainviews,
@@ -179,8 +181,6 @@ class Window(Gtk.ApplicationWindow):
 
         self._headerbar.props.state = HeaderBar.State.MAIN
         self._headerbar.show()
-
-        self._player_toolbar.show_all()
 
         def songs_available_cb(available):
             if available:
@@ -266,6 +266,9 @@ class Window(Gtk.ApplicationWindow):
 
         self.views[View.SEARCH].bind_property(
             'search-state', self._searchbar, 'search-state')
+
+        # Hide the player toolbar on startup.
+        self._player_toolbar.hide()
 
     @log
     def _select_all(self, action=None, param=None):
@@ -471,10 +474,6 @@ class Window(Gtk.ApplicationWindow):
 
     @log
     def _on_selection_mode_changed(self, widget, data=None):
-        if self.props.selection_mode:
-            self._player_toolbar.hide()
-        elif self._player.props.playing:
-            self._player_toolbar.show()
         if not self.props.selection_mode:
             self._on_changes_pending()
 
