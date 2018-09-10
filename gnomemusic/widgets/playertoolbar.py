95f9052c67b9e001addef0919b44c530fc9ce34b
Marinus Schraal <mschraal@gnome.org>
2018-09-10 02:27:52 +0200

    playertoolbar: Simplify repeat mode icon code

--- gnomemusic/widgets/playertoolbar.py
+++ gnomemusic/widgets/playertoolbar.py
@@ -62,6 +62,13 @@ class PlayerToolbar(Gtk.ActionBar):
     _song_info_box = Gtk.Template.Child()
     _title_label = Gtk.Template.Child()
 
+    _repeat_dict = {
+        RepeatMode.ALL: 'media-playlist-repeat-symbolic',
+        RepeatMode.NONE: 'media-playlist-consecutive-symbolic',
+        RepeatMode.SHUFFLE: 'media-playlist-shuffle-symbolic',
+        RepeatMode.SONG: 'media-playlist-repeat-song-symbolic'
+    }
+
     def __repr__(self):
         return '<PlayerToolbar>'
 
@@ -139,16 +146,7 @@ class PlayerToolbar(Gtk.ActionBar):
 
     @log
     def _sync_repeat_image(self, player=None):
-        icon = None
-        if self._player.props.repeat_mode == RepeatMode.NONE:
-            icon = 'media-playlist-consecutive-symbolic'
-        elif self._player.props.repeat_mode == RepeatMode.SHUFFLE:
-            icon = 'media-playlist-shuffle-symbolic'
-        elif self._player.props.repeat_mode == RepeatMode.ALL:
-            icon = 'media-playlist-repeat-symbolic'
-        elif self._player.props.repeat_mode == RepeatMode.SONG:
-            icon = 'media-playlist-repeat-song-symbolic'
-
+        icon = self._repeat_dict[self._player.props.repeat_mode]
         self._repeat_image.set_from_icon_name(icon, Gtk.IconSize.MENU)
 
     @log
