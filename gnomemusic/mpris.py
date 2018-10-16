e2d0712431a5d8a01185f0db63ffcde589d7c6e2
Jean Felder <jfelder@src.gnome.org>
2018-10-16 23:18:17 +0200

    mpris: Replace exceptions with logger messages

--- gnomemusic/mpris.py
+++ gnomemusic/mpris.py
@@ -765,10 +765,8 @@ class MPRIS(DBusInterface):
         elif interface_name == 'org.freedesktop.DBus.Introspectable':
             return {}
         else:
-            raise Exception(
-                'org.mpris.MediaPlayer2.GnomeMusic',
-                'This object does not implement the %s interface'
-                % interface_name)
+            logger.warning(
+                'MPRIS does not implement {} interface'.format(interface_name))
 
     def Set(self, interface_name, property_name, new_value):
         if interface_name == MPRIS.MEDIA_PLAYER2_IFACE:
@@ -792,10 +790,8 @@ class MPRIS(DBusInterface):
                 else:
                     self.player.props.repeat_mode = RepeatMode.NONE
         else:
-            raise Exception(
-                'org.mpris.MediaPlayer2.GnomeMusic',
-                'This object does not implement the %s interface'
-                % interface_name)
+            logger.warning(
+                'MPRIS does not implement {} interface'.format(interface_name))
 
     def PropertiesChanged(self, interface_name, changed_properties,
                           invalidated_properties):
