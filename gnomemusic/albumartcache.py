cb4e0d9e5e8e3846c3fc1262273f476bc7fbcd60
Jean Felder <jfelder@src.gnome.org>
2019-01-29 23:06:49 +0100

    albumartcache: Remove XLARGE size
    This size is never used.

--- gnomemusic/albumartcache.py
+++ gnomemusic/albumartcache.py
@@ -279,7 +279,6 @@ class Art(GObject.GObject):
         SMALL = (48, 48)
         MEDIUM = (128, 128)
         LARGE = (256, 256)
-        XLARGE = (512, 512)
 
         def __init__(self, width, height):
             """Intialize width and height"""
