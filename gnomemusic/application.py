GNOME/gnome-music

commit 2c9c3f17b8a0c06a3573e6607557135b492dba93

application: Use Gtk.Template for about dialog
Create an AboutDialog class and move it to a new file, use Gtk.Template for
loading the ui file and adapt Application accordingly.

Related: #183

gnomemusic/application.py
@@ -36,6 +36,7 @@ from gi.repository import Gtk, Gio, GLib, Gdk
 
 from gnomemusic import log
 from gnomemusic.mpris import MediaPlayer2Service
+from gnomemusic.widgets.aboutdialog import AboutDialog
 from gnomemusic.window import Window
 
 
@@ -87,15 +88,8 @@ class Application(Gtk.Application):
 
     @log
     def _about(self, action, param):
-        def about_response(dialog, response):
-            dialog.destroy()
-
-        builder = Gtk.Builder()
-        builder.add_from_resource('/org/gnome/Music/AboutDialog.ui')
-        about = builder.get_object('about_dialog')
-        about.set_transient_for(self._window)
-        about.connect("response", about_response)
-        about.show()
+        about = AboutDialog()
+        about.props.transient_for = self._window
 
     @log
     def do_startup(self):

data/AboutDialog.ui.in
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <interface>
   <!-- interface-requires gtk+ 3.0 -->
-  <object class="GtkAboutDialog" id="about_dialog">
+  <template class="AboutDialog" parent="GtkAboutDialog">
     <property name="can_focus">False</property>
     <property name="modal">True</property>
     <property name="program_name">@PROGRAM_NAME@</property>
@@ -147,5 +147,5 @@ Yuri Myasoedov
     </property>
     <property name="logo_icon_name">@APPID@</property>
     <property name="wrap_license">True</property>
-  </object>
+  </template>
 </interface>

gnomemusic/widgets/aboutdialog.py
@@ -1,0 +1,49 @@
+# Copyright 2018 The GNOME Music developers
+#
+# GNOME Music is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# GNOME Music is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with GNOME Music; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# The GNOME Music authors hereby grant permission for non-GPL compatible
+# GStreamer plugins to be used and distributed together with GStreamer
+# and GNOME Music.  This permission is above and beyond the permissions
+# granted by the GPL license by which GNOME Music is covered.  If you
+# modify this code, you may extend this exception to your version of the
+# code, but you are not obligated to do so.  If you do not wish to do so,
+# delete this exception statement from your version.
+
+from gi.repository import Gtk
+
+from gnomemusic import log
+
+
+@Gtk.Template(resource_path='/org/gnome/Music/AboutDialog.ui')
+class AboutDialog(Gtk.AboutDialog):
+    """About dialog"""
+
+    __gtype_name__ = 'AboutDialog'
+
+    def __repr__(self):
+        return '<AboutDialog>'
+
+    @log
+    def __init__(self):
+        super().__init__()
+
+        self.connect("response", self._about_response)
+
+        self.show()
+
+    @log
+    def _about_response(self, klass, data=None):
+        klass.destroy()

commit 931ccbace4adeb76117d4c961e3d826a347a03e2

application: Set modal property programmatically
Modal property has to be set after transient_for, otherwise the latter has
no effect. Hence it cannot be done in ui file.

Fixes a regression from 2c9c3f17b8a0c06a3573e6607557135b492dba93

Fixes #261

data/ui/AboutDialog.ui.in
@@ -3,7 +3,6 @@
   <!-- interface-requires gtk+ 3.0 -->
   <template class="AboutDialog" parent="GtkAboutDialog">
     <property name="can_focus">False</property>
-    <property name="modal">True</property>
     <property name="program_name">@PROGRAM_NAME@</property>
     <property name="version">@PACKAGE_VERSION@</property>
     <property name="copyright" translatable="yes">Copyright © 2018 GNOME Music Developers</property>

gnomemusic/application.py
@@ -91,6 +91,8 @@ class Application(Gtk.Application):
     def _about(self, action, param):
         about = AboutDialog()
         about.props.transient_for = self._window
+        # This has to be set after transient_for
+        about.props.modal = True
 
     @log
     def do_startup(self):