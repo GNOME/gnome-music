5bf46b21ebcdc84acf6b96ab7ed7728cb986a984
Sumaid <sumaidsyed@gmail.com>
2019-03-24 20:36:02 +0530

    utils: Optional use of filename as title
    Title metadata filled by tracker is not reliable.
    
    If there's no title tag, title is assigned as filename
    retrieved from url of the file.
    
    Fixes https://gitlab.gnome.org/GNOME/gnome-music/issues/223

--- gnomemusic/utils.py
+++ gnomemusic/utils.py
@@ -25,6 +25,7 @@
 from enum import IntEnum
 
 from gettext import gettext as _
+from gi.repository import GLib
 
 
 class View(IntEnum):
@@ -83,8 +84,14 @@ def get_media_title(item):
     :rtype:
     """
 
-    return (item.get_title()
-            or _("Untitled"))
+    title = item.get_title()
+
+    if not title:
+        url = item.get_url()
+        filename = GLib.filename_from_uri(url)
+        title = GLib.filename_display_basename(filename[0])
+
+    return title.replace("_", " ")
 
 
 def get_media_year(item):
