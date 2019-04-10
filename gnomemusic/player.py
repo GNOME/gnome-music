b6da22fc2d19edda0485c8083eff4928258d46e2
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2019-04-10 12:13:02 -0300

    player: Only initialize Gst once
    A small harmless point noticed while cleaning this code,
    we are initializing Gst twice. This is harmless, but it
    is good to tight things up.
    
    Make Gst be initialized by GstPlayer, and GstPbutils, by
    PlayperPlaylist.

--- gnomemusic/player.py
+++ gnomemusic/player.py
@@ -33,7 +33,7 @@ import gi
 gi.require_version('Gst', '1.0')
 gi.require_version('GstAudio', '1.0')
 gi.require_version('GstPbutils', '1.0')
-from gi.repository import Gio, GLib, GObject, Grl, Gst, GstPbutils
+from gi.repository import Gio, GLib, GObject, Grl, GstPbutils
 
 from gnomemusic import log
 from gnomemusic.gstplayer import GstPlayer, Playback
@@ -97,6 +97,9 @@ class PlayerPlaylist(GObject.GObject):
     @log
     def __init__(self):
         super().__init__()
+
+        GstPbutils.pb_utils_init()
+
         self._songs = []
         self._shuffle_indexes = []
         self._current_index = 0
@@ -578,9 +581,6 @@ class Player(GObject.GObject):
             | GObject.BindingFlags.BIDIRECTIONAL)
 
         self._new_clock = True
-
-        Gst.init(None)
-        GstPbutils.pb_utils_init()
 
         self._gst_player = GstPlayer(application)
         self._gst_player.connect('clock-tick', self._on_clock_tick)
