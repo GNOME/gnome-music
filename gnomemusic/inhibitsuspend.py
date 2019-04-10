f0b31c590fb9176a11de2b12707d72144b853674
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2019-04-10 11:45:13 -0300

    inhibitsuspend: Receive a GtkApplication
    Instead of receiving the toplevel directly, receive
    the application.

--- gnomemusic/inhibitsuspend.py
+++ gnomemusic/inhibitsuspend.py
@@ -43,10 +43,10 @@ class InhibitSuspend(GObject.GObject):
         return '<InhibitSuspend>'
 
     @log
-    def __init__(self, root_window, player):
+    def __init__(self, application, player):
         super().__init__()
 
-        self._application = root_window.get_application()
+        self._application = application
         self._player = player
         self._inhibit_cookie = 0
 
