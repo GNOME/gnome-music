a9caaa48d3735cb8853390a755501b708223fa7f
Christopher Davis <brainblasted@disroot.org>
2019-04-11 12:32:31 -0400

    mpris: Correctly report the DesktopEntry for both profiles
    Music was launching the wrong profile if you clicked on the
    MPRIS entry. This is because the app was always reporting
    itself as the stable version. Now we account for all profiles
    by using the application id.
    
    Closes #273

--- gnomemusic/mpris.py
+++ gnomemusic/mpris.py
@@ -32,6 +32,7 @@ import gnomemusic.utils as utils
 
 from gi.repository import GLib
 from gi.repository import Gio
+from gi.repository import Gtk
 from gnomemusic import log
 import logging
 logger = logging.getLogger(__name__)
@@ -689,7 +690,7 @@ class MediaPlayer2Service(Server):
                 'CanRaise': GLib.Variant('b', True),
                 'HasTrackList': GLib.Variant('b', True),
                 'Identity': GLib.Variant('s', 'Music'),
-                'DesktopEntry': GLib.Variant('s', 'org.gnome.Music'),
+                'DesktopEntry': GLib.Variant('s', self.app.props.application_id),
                 'SupportedUriSchemes': GLib.Variant('as', [
                     'file'
                 ]),
