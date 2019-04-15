1672a558df413d35abb5f14259354473fba5c0da
Jean Felder <jfelder@src.gnome.org>
2019-04-15 08:54:41 +0200

    mpris: Do not interact with playlist view
    MPRIS should not directly interact with a view.

--- gnomemusic/mpris.py
+++ gnomemusic/mpris.py
@@ -263,9 +263,11 @@ class MPRIS(DBusInterface):
         self.player_toolbar = app.get_active_window()._player_toolbar
         self.player_toolbar.connect(
             'thumbnail-updated', self._on_thumbnail_updated)
-        playlists = Playlists.get_default()
-        playlists.connect('playlist-created', self._on_playlists_count_changed)
-        playlists.connect('playlist-deleted', self._on_playlists_count_changed)
+        self._playlists = Playlists.get_default()
+        self._playlists.connect(
+            'playlist-created', self._on_playlists_count_changed)
+        self._playlists.connect(
+            'playlist-deleted', self._on_playlists_count_changed)
         grilo.connect('ready', self._on_grilo_ready)
         self._stored_playlists = []
         self._player_previous_type = None
@@ -656,7 +658,7 @@ class MPRIS(DBusInterface):
 
     def ActivatePlaylist(self, playlist_path):
         playlist_id = self._get_playlist_from_dbus_path(playlist_path).get_id()
-        self.app._window.views[View.PLAYLIST].activate_playlist(playlist_id)
+        self._playlists.activate_playlist(playlist_id)
 
     def GetPlaylists(self, index, max_count, order, reverse):
         """Gets a set of playlists (MPRIS Method).
