4addb65aa9dc18158f109b9fea4d277c52f1e2ff
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2019-04-15 12:14:11 -0300

    application: Present, not show
    The About dialog is a toplevel, and as such, it is
    not "shown", it is "presented". To make it a tiny
    bit more explicit, use the GtkWindow.present method.

--- gnomemusic/application.py
+++ gnomemusic/application.py
@@ -119,7 +119,7 @@ class Application(Gtk.Application):
     def _about(self, action, param):
         about = AboutDialog()
         about.props.transient_for = self._window
-        about.show()
+        about.present()
 
     @log
     def do_startup(self):
