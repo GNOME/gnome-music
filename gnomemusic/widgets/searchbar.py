b468775ed0c011e59cf19a121a319fd00008570a
Reuben Dsouza <reubendsouza800@gmail.com>
2019-04-15 18:02:09 +0530

    Rename Searchbar to SearchBar
    Searchbar is derived from Gtk.SearchBar. The lowercase b in retrospect
    seems like a mistake. ui filenames should follow CamelCase notation.
    Rename Searchbar to SearchBar in the source and rename Searchbar.ui
    to SearchBar.ui.
    
    Closes:https://gitlab.gnome.org/GNOME/gnome-music/issues/271

--- gnomemusic/widgets/searchbar.py
+++ gnomemusic/widgets/searchbar.py
@@ -320,12 +320,12 @@ class DropDown(Gtk.Revealer):
         return grilo_id == "grl-tracker-source"
 
 
-@Gtk.Template(resource_path="/org/gnome/Music/ui/Searchbar.ui")
-class Searchbar(Gtk.SearchBar):
+@Gtk.Template(resource_path="/org/gnome/Music/ui/SearchBar.ui")
+class SearchBar(Gtk.SearchBar):
     """Widget containing the search entry
     """
 
-    __gtype_name__ = 'Searchbar'
+    __gtype_name__ = 'SearchBar'
 
     _search_entry = Gtk.Template.Child()
     _drop_down_button = Gtk.Template.Child()
@@ -334,11 +334,11 @@ class Searchbar(Gtk.SearchBar):
     stack = GObject.Property(type=Gtk.Stack)
 
     def __repr__(self):
-        return '<Searchbar>'
+        return '<SearchBar>'
 
     @log
     def __init__(self):
-        """Initialize the Searchbar"""
+        """Initialize the SearchBar"""
         super().__init__()
 
         self._timeout = None
