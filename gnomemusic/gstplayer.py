7b90610832d45332248f88a5779e3636bb521d58
Marinus Schraal <mschraal@gnome.org>
2019-05-04 13:55:05 +0200

    gstplayer: Do not seek to exact end
    Seeking to the exact end of a song in combination with gapless playback
    triggers a stream error.
    Make sure to never seek to the exact endpoint of a stream.

--- gnomemusic/gstplayer.py
+++ gnomemusic/gstplayer.py
@@ -297,10 +297,13 @@ class GstPlayer(GObject.GObject):
 
         :param float seconds: Position in seconds to seek
         """
+        if seconds > 0:
+            seconds = (seconds * Gst.SECOND) - 1
+
         # FIXME: seek should be signalled to MPRIS
         self._player.seek_simple(
             Gst.Format.TIME, Gst.SeekFlags.FLUSH | Gst.SeekFlags.KEY_UNIT,
-            seconds * Gst.SECOND)
+            seconds)
 
     @log
     def _start_plugin_installation(
