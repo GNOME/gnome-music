1a488464dffd8a91e8934fe5a963f3118291010f
Marinus Schraal <mschraal@gnome.org>
2019-05-08 16:12:35 +0200

    coregrilo: Fill model

--- gnomemusic/coregrilo.py
+++ gnomemusic/coregrilo.py
@@ -5,11 +5,32 @@ from gi.repository import Grl, GObject
 
 class CoreGrilo(GObject.GObject):
 
+    METADATA_KEYS = [
+        Grl.METADATA_KEY_ALBUM,
+        Grl.METADATA_KEY_ALBUM_ARTIST,
+        Grl.METADATA_KEY_ALBUM_DISC_NUMBER,
+        Grl.METADATA_KEY_ARTIST,
+        Grl.METADATA_KEY_CREATION_DATE,
+        Grl.METADATA_KEY_COMPOSER,
+        Grl.METADATA_KEY_DURATION,
+        Grl.METADATA_KEY_FAVOURITE,
+        Grl.METADATA_KEY_ID,
+        Grl.METADATA_KEY_LYRICS,
+        Grl.METADATA_KEY_PLAY_COUNT,
+        Grl.METADATA_KEY_THUMBNAIL,
+        Grl.METADATA_KEY_TITLE,
+        Grl.METADATA_KEY_TRACK_NUMBER,
+        Grl.METADATA_KEY_URL
+    ]
+
     def __repr__(self):
         return "<CoreGrilo>"
 
-    def __init__(self):
+    def __init__(self, model, table):
         super().__init__()
+
+        self._model = model
+        self._table = table
 
         Grl.init(None)
 
@@ -29,7 +50,46 @@ class CoreGrilo(GObject.GObject):
         print(source.props.source_id)
         if source.props.source_id == "grl-tracker-source":
             self._tracker_source = source
+            self._tracker_initial_fill(source)
             print(self._tracker_source, "added")
 
     def _on_source_removed(self, registry, source):
         print("removed", source.props.source_id)
+
+    def _tracker_initial_fill(self, source):
+        query = """
+            SELECT
+                rdf:type(?song)
+                ?song AS ?tracker_urn
+                nie:title(?song) AS ?title
+                tracker:id(?song) AS ?id
+                ?song
+                nie:url(?song) AS ?url
+                nie:title(?song) AS ?title
+                nmm:artistName(nmm:performer(?song)) AS ?artist
+                nie:title(nmm:musicAlbum(?song)) AS ?album
+                nfo:duration(?song) AS ?duration
+                nie:usageCounter(?song) AS ?play_count
+            WHERE { ?song a nmm:MusicPiece . }
+        """.replace('\n', ' ').strip()
+
+        print(query)
+
+        options = self._fast_options.copy()
+
+        self._tracker_source.query(
+            query, self.METADATA_KEYS, options, self._add_to_model)
+
+    def _add_to_model(self, source, op_id, media, user_data, error):
+        if error:
+            print("ERROR", error)
+            return
+
+        if not media:
+            print("NO MEDIA", source, op_id, media, error)
+            return
+
+        self._model.append(media)
+        self._table[media.get_id()] = media
+
+        # print(media.get_url(), media.get_title())
