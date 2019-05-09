1a24be03de1cd60d5d50d282fe00934b476e866a
Marinus Schraal <mschraal@gnome.org>
2019-05-09 17:11:39 +0200

    coremodel: Use dzl listmodelfilter

--- gnomemusic/coremodel.py
+++ gnomemusic/coremodel.py
@@ -1,4 +1,6 @@
-from gi.repository import GObject, Gio
+import gi
+gi.require_version('Dazzle', '1.0')
+from gi.repository import Dazzle, GObject, Gio
 
 from gnomemusic import log
 from gnomemusic.coregrilo import CoreGrilo
@@ -25,26 +27,28 @@ class CoreModel(GObject.GObject):
 
     @log
     def get_album_model(self, media):
-        self._album_store = Gio.ListStore()
-        album_id = media.get_id()
+        albums_ids = []
+
+        model_filter = Dazzle.ListModelFilter.new(self._model)
+        model_filter.set_filter_func(lambda a: False)
+
+        def _filter_func(core_song):
+            return core_song._media.get_id() in albums_ids
 
         def _reverse_sort(song_a, song_b):
             return song_b.props.track_number - song_a.props.track_number
 
         def _callback(source, dunno, media, something, something2):
             if media is None:
-                self._album_store.sort(_reverse_sort)
+                model_filter.set_filter_func(_filter_func)
                 return
 
-            print("media", media)
-
-            song = self._hash[media.get_id()]
-            self._album_store.append(song)
+            albums_ids.append(media.get_id())
 
         # For POC sake, use old grilo
         grilo.populate_album_songs(media, _callback)
 
-        return self._album_store
+        return model_filter
 
     @log
     def _on_media_removed(self, klass, media):
