f989f20cdcebe7100dc12aff82e36f9bf29f1171
Marinus Schraal <mschraal@gnome.org>
2019-05-09 23:13:51 +0200

    albumwidget2: Hackish way of getting play working

--- gnomemusic/widgets/albumwidget2.py
+++ gnomemusic/widgets/albumwidget2.py
@@ -63,6 +63,8 @@ class AlbumWidget2(Gtk.EventBox):
         self.bind_property(
             'selected-items-count', self._parent_view, 'selected-items-count')
 
+        self._listbox.connect("row-activated", self._on_row_activated)
+
     @log
     def update(self, album):
         """Update the album widget.
@@ -138,6 +140,33 @@ class AlbumWidget2(Gtk.EventBox):
         mins = (total_duration // 60) + 1
         self._running_info_label.props.label = ngettext(
             "{} minute", "{} minutes", mins).format(mins)
+
+    @log
+    def _on_row_activated(self, klass, value):
+        old_model = Gtk.ListStore(
+            GObject.TYPE_STRING,  # title
+            GObject.TYPE_STRING,
+            GObject.TYPE_STRING,
+            GObject.TYPE_STRING,
+            GdkPixbuf.Pixbuf,    # icon
+            GObject.TYPE_OBJECT,  # song object
+            GObject.TYPE_BOOLEAN,  # item selected
+            GObject.TYPE_STRING,
+            GObject.TYPE_BOOLEAN,
+            GObject.TYPE_INT,  # icon shown
+            GObject.TYPE_BOOLEAN,
+            GObject.TYPE_INT
+        )
+
+        for song in self._model:
+            _iter = old_model.insert_with_valuesv(-1, [5], [song.props.media])
+            if song is self._model[value.get_index()]:
+                activated_iter = _iter
+
+        self._player.set_playlist(
+            PlayerPlaylist.Type.ALBUM, self._album_name, old_model,
+            activated_iter)
+        self._player.play()
 
     @log
     def _on_selection_changed(self, klass, value):
