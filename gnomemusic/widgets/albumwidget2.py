70216cc7c67ed00eda051e54ed2bc840f6f4f298
Marinus Schraal <mschraal@gnome.org>
2019-06-02 10:37:16 +0200

    albumwidget2: Make it work with ListBox as well

--- gnomemusic/widgets/albumwidget2.py
+++ gnomemusic/widgets/albumwidget2.py
@@ -84,30 +84,44 @@ class AlbumWidget2(Gtk.EventBox):
         self._set_composer_label(album)
 
         self._album = album
-        self._model = self._parent_view._window._app._coremodel.get_album_model(album)
-        self._listbox.bind_model(self._model[0], self._create_widget)
+        self._album_model = self._parent_view._window._app._coremodel.get_album_model(album)
+        self._listbox.bind_model(self._album_model, self._create_widget)
 
-    @log
-    def _create_widget(self, song):
-        song_widget = SongWidget(song.props.media)
+    def _create_widget(self, disc):
+        disc_box = self._create_disc_box(
+            disc.media.get_album_disc_number(), disc.model)
 
-        song.bind_property(
-            "favorite", song_widget, "favorite",
-            GObject.BindingFlags.BIDIRECTIONAL
-            | GObject.BindingFlags.SYNC_CREATE)
-        song.bind_property(
-            "selected", song_widget, "selected",
-            GObject.BindingFlags.BIDIRECTIONAL
-            | GObject.BindingFlags.SYNC_CREATE)
+        return disc_box
 
-        self.bind_property(
-            "selection-mode", song_widget, "selection-mode")
+    def _create_disc_box(self, disc_nr, album_model):
+        disc_box = DiscBox(None, album_model)
+        # disc_box.set_songs(disc_songs)
+        disc_box.set_disc_number(disc_nr)
+        disc_box.props.columns = 2
+        disc_box.props.show_durations = False
+        disc_box.props.show_favorites = False
+        disc_box.props.show_song_numbers = True
+        disc_box.connect('song-activated', self._song_activated)
 
-        song.connect("notify::selected", self._on_selection_changed)
+        return disc_box
 
-        self._set_duration_label()
+        # song.bind_property(
+        #     "favorite", song_widget, "favorite",
+        #     GObject.BindingFlags.BIDIRECTIONAL
+        #     | GObject.BindingFlags.SYNC_CREATE)
+        # song.bind_property(
+        #     "selected", song_widget, "selected",
+        #     GObject.BindingFlags.BIDIRECTIONAL
+        #     | GObject.BindingFlags.SYNC_CREATE)
 
-        return song_widget
+        # self.bind_property(
+        #     "selection-mode", song_widget, "selection-mode")
+
+        # song.connect("notify::selected", self._on_selection_changed)
+
+        # self._set_duration_label()
+
+        # return song_widget
 
     @log
     def _set_composer_label(self, album):
