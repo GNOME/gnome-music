GNOME/gnome-music

commit a7c6ce8e5b1b3682b21430873f69d91859ead4f7

mpris: Handle playlists renaming
Send a PlaylistChanged signal from the MediaPlayer2.Playlists
interface when a playlist is renamed.

gnomemusic/mpris.py
@@ -251,6 +251,7 @@ class MediaPlayer2Service(Server):
             'playlist-created', self._on_playlists_count_changed)
         self._playlists.connect(
             'playlist-deleted', self._on_playlists_count_changed)
+        self._playlists.connect('playlist-renamed', self._on_playlist_renamed)
         grilo.connect('ready', self._on_grilo_ready)
         self._stored_playlists = []
         self._player_previous_type = None
@@ -519,6 +520,15 @@ class MediaPlayer2Service(Server):
         self._reload_playlists()
 
     @log
+    def _on_playlist_renamed(self, playlists, renamed_playlist):
+        mpris_playlist = self._get_mpris_playlist_from_playlist(
+            renamed_playlist)
+        self.con.emit_signal(
+            None, '/org/mpris/MediaPlayer2',
+            MPRIS.MEDIA_PLAYER2_PLAYLISTS_IFACE, 'PlaylistChanged',
+            GLib.Variant.new_tuple(GLib.Variant('(oss)', mpris_playlist)))
+
+    @log
     def _on_grilo_ready(self, grilo):
         self._reload_playlists()
 
@@ -668,13 +678,6 @@ class MediaPlayer2Service(Server):
         if first_index < 0:
             first_index = None
         return mpris_playlists[index + max_count - 1:first_index:-1]
-
-    def PlaylistChanged(self, playlist):
-        self.con.emit_signal(None,
-                             '/org/mpris/MediaPlayer2',
-                             MediaPlayer2Service.MEDIA_PLAYER2_PLAYLISTS_IFACE,
-                             'PlaylistChanged',
-                             GLib.Variant.new_tuple(GLib.Variant('(oss)', playlist)))
 
     def Get(self, interface_name, property_name):
         return self.GetAll(interface_name)[property_name]

commit 461a9e1b9a60396f9d909dfce49a8570c48c7405

mpris: Fix interface name in _on_playlist_renamed
This regression was introduced by commit
a7c6ce8e5b1b3682b21430873f69d91859ead4f7.

gnomemusic/mpris.py
@@ -526,7 +526,8 @@ class MediaPlayer2Service(Server):
             renamed_playlist)
         self.con.emit_signal(
             None, '/org/mpris/MediaPlayer2',
-            MPRIS.MEDIA_PLAYER2_PLAYLISTS_IFACE, 'PlaylistChanged',
+            MediaPlayer2Service.MEDIA_PLAYER2_PLAYLISTS_IFACE,
+            'PlaylistChanged',
             GLib.Variant.new_tuple(GLib.Variant('(oss)', mpris_playlist)))
 
     @log