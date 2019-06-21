eb949b3d680405577b04e078a842653257679795
Marinus Schraal <mschraal@gnome.org>
2019-06-21 16:22:08 +0200

    coremodel: Move CoreDisc to a seperate source

--- gnomemusic/coremodel.py
+++ gnomemusic/coremodel.py
@@ -5,6 +5,7 @@ from gi._gi import pygobject_new_full
 
 from gnomemusic import log
 from gnomemusic.coreartist import CoreArtist
+from gnomemusic.coredisc import CoreDisc
 from gnomemusic.coregrilo import CoreGrilo
 from gnomemusic.coresong import CoreSong
 from gnomemusic.grilo import grilo
@@ -24,32 +25,6 @@ from gnomemusic.widgets.songwidget import SongWidget
 # For the playlist model, the CoreArtist or CoreAlbum derived discs are
 # flattened and recreated as a new model. This is to allow for multiple
 # occurences of the same song: same grilo id, but unique object.
-
-
-class CoreDisc(GObject.GObject):
-
-    duration = GObject.Property(type=int, default=None)
-    media = GObject.Property(type=Grl.Media, default=None)
-    model = GObject.Property(type=Gio.ListModel, default=None)
-
-    def __init__(self, media, model):
-        super().__init__()
-
-        self.props.model = model
-        self.update(media)
-
-        self.props.model.connect("items-changed", self._on_list_items_changed)
-
-    def update(self, media):
-        self.props.media = media
-
-    def _on_list_items_changed(self, model, pos, removed, added):
-        duration = 0
-
-        for coresong in model:
-            duration += coresong.props.duration
-
-        self.props.duration = duration
 
 
 class CoreArtistAlbum(GObject.GObject):
