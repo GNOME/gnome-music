dd3ac5a8071b73ad19caf6ccb7c7294eaa8372af
Marinus Schraal <mschraal@gnome.org>
2019-06-26 16:35:04 +0200

    artistview: Make selection mode work

--- gnomemusic/coreartist.py
+++ gnomemusic/coreartist.py
@@ -12,7 +12,6 @@ class CoreArtist(GObject.GObject):
 
     artist = GObject.Property(type=str)
     media = GObject.Property(type=Grl.Media)
-    selected = GObject.Property(type=bool, default=False)
 
     @log
     def __init__(self, media, coremodel):
@@ -20,6 +19,7 @@ class CoreArtist(GObject.GObject):
 
         self._coremodel = coremodel
         self._model = None
+        self._selected = False
 
         self.update(media)
 
@@ -33,5 +33,28 @@ class CoreArtist(GObject.GObject):
         if self._model is None:
             self._model = self._coremodel.get_artists_model_full(
                 self.props.media)
+            self._model.connect("items-changed", self._on_items_changed)
+
+        self._on_items_changed(self._model, None, None, None)
 
         return self._model
+
+    def _on_items_changed(self, model, pos, removed, added):
+        with self.freeze_notify():
+            for corealbum in self._model:
+                corealbum.props.selected = self.props.selected
+
+    @GObject.Property(type=bool, default=False)
+    def selected(self):
+        return self._selected
+
+    @selected.setter
+    def selected(self, value):
+        self._selected = value
+
+        # The model is loaded on-demand, so the first time the model is
+        # returned it can still be empty. This is problem for returning
+        # a selection. Trigger loading of the model here if a selection
+        # is requested, it will trigger the filled model update as
+        # well.
+        self.props.model
