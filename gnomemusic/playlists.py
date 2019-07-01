b68b8a4750767bbd46f528f459ec5fef14c291fd
Jean Felder <jfelder@src.gnome.org>
2019-07-01 23:40:11 +0200

    playlists: Remove unused signals
    playlist-created and playlist-deleted signals are not used anymore.

--- gnomemusic/playlists.py
+++ gnomemusic/playlists.py
@@ -159,10 +159,6 @@ class Playlists(GObject.GObject):
     __gsignals__ = {
         "activate-playlist": (
             GObject.SignalFlags.RUN_FIRST, None, (Playlist,)),
-        'playlist-created': (
-            GObject.SignalFlags.RUN_FIRST, None, (Grl.Media,)
-        ),
-        "playlist-deleted": (GObject.SignalFlags.RUN_FIRST, None, (str,)),
         "playlist-updated": (GObject.SignalFlags.RUN_FIRST, None, (Playlist,)),
         "playlist-renamed": (GObject.SignalFlags.RUN_FIRST, None, (Playlist,)),
         'song-added-to-playlist': (
@@ -418,7 +414,6 @@ class Playlists(GObject.GObject):
                 creation_date=item.get_creation_date())
             self._playlists_model.insert_sorted(
                 new_playlist, Playlist.compare_playlist_func)
-            self.emit("playlist-created", item)
             callback(new_playlist)
 
         def cursor_callback(cursor, res, data):
@@ -476,7 +471,6 @@ class Playlists(GObject.GObject):
         """
         def update_callback(conn, res, data):
             conn.update_finish(res)
-            self.emit("playlist-deleted", playlist.props.pl_id)
 
         self._pls_todelete.remove(playlist)
         query = Query.delete_playlist(playlist.props.pl_id)
