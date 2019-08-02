e8510cf66aa434fc2213a94fca90d1ee1f6349f7
Marinus Schraal <mschraal@gnome.org>
2019-08-02 13:03:25 +0200

    start of retrieving artist art

---
+++ gnomemusic/widgets/artistartstack.py
@@ -0,0 +1,120 @@
+# Copyright 2019 The GNOME Music developers
+#
+# GNOME Music is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# GNOME Music is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with GNOME Music; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# The GNOME Music authors hereby grant permission for non-GPL compatible
+# GStreamer plugins to be used and distributed together with GStreamer
+# and GNOME Music.  This permission is above and beyond the permissions
+# granted by the GPL license by which GNOME Music is covered.  If you
+# modify this code, you may extend this exception to your version of the
+# code, but you are not obligated to do so.  If you do not wish to do so,
+# delete this exception statement from your version.
+
+from gi.repository import GObject, Gtk
+
+from gnomemusic import log
+from gnomemusic.albumartcache import Art
+from gnomemusic.artistart import ArtistCache, DefaultIcon
+from gnomemusic.coreartist import CoreArtist
+
+
+class ArtistArtStack(Gtk.Stack):
+    """Provides a smooth transition between image states
+
+    Uses a Gtk.Stack to provide an in-situ transition between an image
+    state. Either between the "loading" state versus the "loaded" state
+    or in between songs.
+    """
+
+    __gtype_name__ = "ArtistArtStack"
+
+    _default_icon = DefaultIcon()
+
+    def __repr__(self):
+        return "ArtistArtStack"
+
+    @log
+    def __init__(self, size=Art.Size.MEDIUM):
+        """Initialize the CoverStack
+
+        :param Art.Size size: The size of the art used for the cover
+        """
+        super().__init__()
+
+        self._size = None
+        self._timeout = None
+
+        self._loading_cover = Gtk.Image()
+        self._cover = Gtk.Image()
+        self._cover = Gtk.Image()
+
+        self.add_named(self._loading_cover, "loading")
+        self.add_named(self._cover, "artist")
+
+        self._active_child = "loading"
+
+        self.props.size = size
+        self.props.transition_type = Gtk.StackTransitionType.CROSSFADE
+        self.props.visible_child_name = "loading"
+
+        self.show_all()
+
+    @GObject.Property(type=object, flags=GObject.ParamFlags.READWRITE)
+    def size(self):
+        """Size of the cover
+
+        :returns: The size used
+        :rtype: Art.Size
+        """
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        """Set the cover size
+
+        :param Art.Size value: The size to use for the cover
+        """
+        self._size = value
+
+        icon = self._default_icon.get(
+            DefaultIcon.Type.LOADING, self.props.size, self.props.scale_factor)
+        self._loading_cover.props.surface = icon
+
+    @GObject.Property(type=CoreArtist, default=None)
+    def coreartist(self):
+        return self._coreartist
+
+    @coreartist.setter
+    def coreartist(self, coreartist):
+        self._coreartist = coreartist
+
+        print("connecting")
+        self._coreartist.connect(
+            "notify::cached-thumbnail-uri", self._on_thumbnail_changed)
+
+        if self._coreartist.props.cached_thumbnail_uri is not None:
+            self._on_thumbnail_changed(self._coreartist, None)
+
+    def _on_thumbnail_changed(self, coreartist, uri):
+        print("thumbnail changed")
+        cache = ArtistCache(self.props.size, self.props.scale_factor)
+        cache.connect("hit", self._on_cache_hit)
+
+        cache.query(coreartist)
+
+    def _on_cache_hit(self, cache, surface):
+        print("surface", surface)
+        self._cover.props.surface = surface
+        self.props.visible_child_name = "artist"
