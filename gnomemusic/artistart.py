efd9e7b35682b83519a570ddb6b6be64a5b3479f
Marinus Schraal <mschraal@gnome.org>
2019-08-04 12:18:28 +0200

    artistart: Access Grilo as property

--- gnomemusic/artistart.py
+++ gnomemusic/artistart.py
@@ -121,10 +121,11 @@ class DefaultIcon(GObject.GObject):
 
 class ArtistArt(GObject.GObject):
 
-    def __init__(self, coreartist):
+    def __init__(self, coreartist, coremodel):
         """Initialize the ArtistArt.
 
         :param CoreArtist coreartist: The coreartist to use
+        :param CoreModel coremodel: The main coremodel
         """
         super().__init__()
 
@@ -134,8 +135,7 @@ class ArtistArt(GObject.GObject):
         if self._in_cache():
             return
 
-        # FIXME: Ugly.
-        grilo = self._coreartist._coremodel._grilo
+        grilo = coremodel.props.grilo
 
         self._coreartist.connect(
             "notify::thumbnail", self._on_thumbnail_changed)
