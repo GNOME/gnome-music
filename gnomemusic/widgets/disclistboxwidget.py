f2c84d3f82d0de0ab71dc50176642fce2943b32a
Marinus Schraal <mschraal@gnome.org>
2019-10-31 16:33:58 +0100

    disclistboxwidget: Disable song widget event

--- gnomemusic/widgets/disclistboxwidget.py
+++ gnomemusic/widgets/disclistboxwidget.py
@@ -89,7 +89,7 @@ class DiscBox(Gtk.ListBoxRow):
             GObject.BindingFlags.BIDIRECTIONAL
             | GObject.BindingFlags.SYNC_CREATE)
 
-        song_widget.connect('button-release-event', self._song_activated)
+        # song_widget.connect('button-release-event', self._song_activated)
 
         row = Gtk.ListBoxRow()
         row.props.activatable = False
