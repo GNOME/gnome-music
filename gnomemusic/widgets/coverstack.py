b81074cc84fe06e7259108d89a9ec753769fa3a5
Marinus Schraal <mschraal@gnome.org>
2019-10-31 14:10:06 +0100

    coverstack: Remove show_all

--- gnomemusic/widgets/coverstack.py
+++ gnomemusic/widgets/coverstack.py
@@ -65,8 +65,6 @@ class CoverStack(Gtk.Stack):
 
         self.connect("destroy", self._on_destroy)
 
-        self.show_all()
-
     @GObject.Property(type=object, flags=GObject.ParamFlags.READWRITE)
     def size(self):
         """Size of the cover
