75ccc3ce3ceb904ce3627fd0fa47c5bb9569bf90
Jean Felder <jfelder@src.gnome.org>
2020-01-02 19:34:07 +0100

    application: Restore keyboard shortcut to open help
    Since the removal of the application menu (commit 9aa739a9), it is no
    longer possible to open Help with the F1 shortcut.
    
    Restore this shortcut by adding the adding the corresponding
    accelerator.
    
    closes: #353

--- gnomemusic/application.py
+++ gnomemusic/application.py
@@ -133,7 +133,7 @@ class Application(Gtk.Application):
     def _set_actions(self):
         action_entries = [
             ('about', self._about, None),
-            ('help', self._help, None),
+            ("help", self._help, ("app.help", ["F1"])),
             ("quit", self._quit, ("app.quit", ["<Ctrl>Q"]))
         ]
 
