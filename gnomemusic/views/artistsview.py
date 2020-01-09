GNOME/gnome-music

commit 74dc676d50dc049827beec308dbcfb133891a076

artisttile: Directly inherit from GtkListBoxRow

gnomemusic/views/artistsview.py
@@ -141,7 +141,6 @@ class ArtistsView(BaseView):
     @log
     def _on_artist_activated(self, sidebar, row, data=None):
         """Initializes new artist album widgets"""
-        artist_tile = row.get_child()
         # On application start the first row of ArtistView is activated
         # to show an intial artist. When this happens while any of the
         # views are in selection mode, this artist will be incorrectly
@@ -150,11 +149,11 @@ class ArtistsView(BaseView):
         # ArtistsView, to circumvent this issue.
         if (self.props.selection_mode
                 and self._window.props.active_view is self):
-            artist_tile.props.selected = not artist_tile.props.selected
+            row.props.selected = not row.props.selected
             return
 
         # Prepare a new artist_albums_widget here
-        coreartist = artist_tile.props.coreartist
+        coreartist = row.props.coreartist
         if coreartist.props.artist in self._loaded_artists:
             scroll_vadjustment = self._view_container.props.vadjustment
             scroll_vadjustment.props.value = 0.

data/ui/ArtistTile.ui
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <interface>
-  <template class="ArtistTile" parent="GtkEventBox">
+  <template class="ArtistTile" parent="GtkListBoxRow">
     <property name="can_focus">False</property>
     <property name="visible">True</property>
     <child>

gnomemusic/widgets/artisttile.py
@@ -29,7 +29,7 @@ from gnomemusic.coreartist import CoreArtist
 
 
 @Gtk.Template(resource_path='/org/gnome/Music/ui/ArtistTile.ui')
-class ArtistTile(Gtk.EventBox):
+class ArtistTile(Gtk.ListBoxRow):
     """Row for sidebars
 
     Contains a label and an optional checkbox.

commit 77a354da456b94b262f80341d7839fbc96ab35d9

artistsview: Correctly access artisttile on toggle_selection
This is a regression introduced by commit
74dc676d50dc049827beec308dbcfb133891a076. ArtistTile now inherits
from a GtkListBoxRow, there is no need to call the `get_child` method
to access it.

gnomemusic/views/artistsview.py
@@ -200,8 +200,7 @@ class ArtistsView(BaseView):
     def _toggle_all_selection(self, selected):
 
         def toggle_selection(child):
-            tile = child.get_child()
-            tile.props.selected = selected
+            child.props.selected = selected
 
         self._sidebar.foreach(toggle_selection)
 