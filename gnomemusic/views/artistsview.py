e2c4642c6554454c9b3f62358ad63d6f5ff594a8
Jean Felder <jfelder@src.gnome.org>
2020-01-10 13:15:30 +0100

    artistsviews: Prevent selection changes if the view is not visible
    A user can only select artists if the view is visible.

--- gnomemusic/views/artistsview.py
+++ gnomemusic/views/artistsview.py
@@ -190,7 +190,8 @@ class ArtistsView(BaseView):
     @log
     def _on_selection_mode_changed(self, widget, data=None):
         selection_mode = self._window.props.selection_mode
-        if selection_mode == self.props.selection_mode:
+        if (selection_mode == self.props.selection_mode
+                or self.get_parent().get_visible_child() != self):
             return
 
         super()._on_selection_mode_changed(widget, data)
