6675eab1d455567b35b05d71b9241e3d1e214e70
Jean Felder <jfelder@src.gnome.org>
2020-01-11 23:09:26 +0100

    coremodel: Add an active_media property to create a player playlist
    Only some widgets and the MPRIS objet can set the playlist player by
    calling the set_player_model() function from the CoreModel. Once the
    player playlist is loaded, the appropriate is loaded and the player
    starts playing.
    Each type of player playlist can be set by only one widget. However, a
    playlist type can also set by the MPRIS object. When PlaylistsWidget
    or MPRIS set a new player playlist, the active_playlist property is
    used to inform the other object of the change.
    
    This commit generalizes this approach by introducing a new
    active_media property. A set_player_model call is replaced by setting
    the active_media. Based on the type of media, _set_player_model() is
    called with the correct parameters.
    
    The next commit will remove the active_playlist property which is
    redundant with the active_media property.

--- gnomemusic/widgets/albumwidget.py
+++ gnomemusic/widgets/albumwidget.py
@@ -27,7 +27,6 @@ from gettext import ngettext
 from gi.repository import GObject, Grl, Gtk
 
 from gnomemusic.albumartcache import Art
-from gnomemusic.player import PlayerPlaylist
 from gnomemusic.widgets.disclistboxwidget import DiscBox
 from gnomemusic.widgets.disclistboxwidget import DiscListBox  # noqa: F401
 
@@ -167,8 +166,7 @@ class AlbumWidget(Gtk.EventBox):
 
         signal_id = self._coremodel.connect(
             "playlist-loaded", _on_playlist_loaded)
-        self._coremodel.set_player_model(
-            PlayerPlaylist.Type.ALBUM, self._album_model)
+        self._coremodel.props.active_media = self._corealbum
 
         return True
 
