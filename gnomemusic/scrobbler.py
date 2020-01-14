9b704ae9f960fa9759531983bb8991a37af7d896
Jean Felder <jfelder@src.gnome.org>
2020-01-14 23:22:32 +0100

    scrobbler: Add a property to get account identity

--- gnomemusic/scrobbler.py
+++ gnomemusic/scrobbler.py
@@ -145,6 +145,15 @@ class GoaLastFM(GObject.GObject):
         """Enable music suport of the Last.fm account"""
         self._account.props.music_disabled = False
 
+    @GObject.Property(type=str, default="", flags=GObject.ParamFlags.READABLE)
+    def identity(self):
+        """Get Last.fm account identity
+
+        :returns: Last.fm account identity
+        :rtype: str
+        """
+        return self._account.props.identity
+
     @GObject.Property
     def secret(self):
         """Retrieve the Last.fm client secret"""
@@ -241,6 +250,15 @@ class LastFmScrobbler(GObject.GObject):
 
     def configure(self):
         self._goa_lastfm.configure()
+
+    @GObject.Property(type=str, default="", flags=GObject.ParamFlags.READABLE)
+    def identity(self):
+        """Get Last.fm account identity
+
+        :returns: Last.fm account identity
+        :rtype: str
+        """
+        return self._goa_lastfm.props.identity
 
     @GObject.Property(type=int, default=GoaLastFM.State.NOT_ENABLED)
     def account_state(self):
