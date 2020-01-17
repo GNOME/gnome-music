GNOME/gnome-music

commit a755b53974e9ab654f3b8f2f845e4732cddc2197

baseview: Do not change selection-mode if the view is not visible
A user can only select songs, albums or artists if the view is
visible.

gnomemusic/views/artistsview.py
@@ -189,6 +189,10 @@ class ArtistsView(BaseView):
 
     @log
     def _on_selection_mode_changed(self, widget, data=None):
+        selection_mode = self._window.props.selection_mode
+        if selection_mode == self.props.selection_mode:
+            return
+
         super()._on_selection_mode_changed(widget, data)
 
         self._view.props.sensitive = not self.props.selection_mode

gnomemusic/views/baseview.py
@@ -66,12 +66,12 @@ class BaseView(Gtk.Stack):
         self.add(self._grid)
         self.show_all()
 
-        self._selection_mode_id = self.connect(
-            "notify::selection-mode", self._on_selection_mode_changed)
-
         self.bind_property(
-            'selection-mode', self._window, 'selection-mode',
-            GObject.BindingFlags.BIDIRECTIONAL)
+            "selection-mode", self._window, "selection-mode",
+            GObject.BindingFlags.DEFAULT)
+
+        self._selection_mode_id = self._window.connect(
+            "notify::selection-mode", self._on_selection_mode_changed)
 
     @log
     def _setup_view(self):
@@ -80,6 +80,11 @@ class BaseView(Gtk.Stack):
 
     @log
     def _on_selection_mode_changed(self, widget, data=None):
-        if (self.get_parent().get_visible_child() == self
-                and not self.props.selection_mode):
+        selection_mode = self._window.props.selection_mode
+        if (selection_mode == self.props.selection_mode
+                or self.get_parent().get_visible_child() != self):
+            return
+
+        self.props.selection_mode = selection_mode
+        if self.props.selection_mode is False:
             self.unselect_all()

gnomemusic/views/playlistsview.py
@@ -97,7 +97,7 @@ class PlaylistsView(BaseView):
             "notify::active-playlist", self._on_active_playlist_changed)
 
         # Selection is only possible from the context menu
-        self.disconnect(self._selection_mode_id)
+        self._window.disconnect(self._selection_mode_id)
 
         self.show_all()
 

gnomemusic/views/songsview.py
@@ -155,6 +155,10 @@ class SongsView(BaseView):
 
     @log
     def _on_selection_mode_changed(self, widget, data=None):
+        selection_mode = self._window.props.selection_mode
+        if selection_mode == self.props.selection_mode:
+            return
+
         super()._on_selection_mode_changed(widget, data)
 
         cols = self._view.get_columns()

commit 4248e9bc6dd4f0caccc20444944f6c3b72111856

baseview: Fix selection-mode changes
This fixes a regresssion introduced by commits
a755b53974e9ab654f3b8f2f845e4732cddc2197,
c61a2469589176f7138d459796085880f2f322d1 and
b5e86fb8693a7c4659f237164f38dcf426384817.

Those commits tried to prevent selection-mode changes when a main view
is not visible. It introduced two changes:
- remove the "selection-mode" bidirectional binding between the
window and the base view and replaced it with a binding from the base
view to the window.
- listen to selection-mode changes from the window to prevent a
selection-mode change of a view if the value is unchanged or if the
view is not the visible view.

This introduced the following regression: the specific selection-mode
changes of ArtistsView and SongsView are not called anymore when
activating selection-mode via a Ctrl + left click. Indeed, in that
case, Window and View already have the same selection-mode value.

It turns out, this issue can be fixed by making two changes:
- restore the bidirectional binding between the Window and the
BaseView.
- simplify the selection-mode change test: test for the visible view
only.

gnomemusic/views/artistsview.py
@@ -189,9 +189,7 @@ class ArtistsView(BaseView):
 
     @log
     def _on_selection_mode_changed(self, widget, data=None):
-        selection_mode = self._window.props.selection_mode
-        if (selection_mode == self.props.selection_mode
-                or self.get_parent().get_visible_child() != self):
+        if self.get_parent().get_visible_child() != self:
             return
 
         super()._on_selection_mode_changed(widget, data)

gnomemusic/views/baseview.py
@@ -67,9 +67,9 @@ class BaseView(Gtk.Stack):
 
         self.bind_property(
             "selection-mode", self._window, "selection-mode",
-            GObject.BindingFlags.DEFAULT)
+            GObject.BindingFlags.BIDIRECTIONAL)
 
-        self._selection_mode_id = self._window.connect(
+        self._selection_mode_id = self.connect(
             "notify::selection-mode", self._on_selection_mode_changed)
 
     @log
@@ -79,11 +79,8 @@ class BaseView(Gtk.Stack):
 
     @log
     def _on_selection_mode_changed(self, widget, data=None):
-        selection_mode = self._window.props.selection_mode
-        if (selection_mode == self.props.selection_mode
-                or self.get_parent().get_visible_child() != self):
+        if self.get_parent().get_visible_child() != self:
             return
 
-        self.props.selection_mode = selection_mode
         if self.props.selection_mode is False:
             self.deselect_all()

gnomemusic/views/playlistsview.py
@@ -97,7 +97,7 @@ class PlaylistsView(BaseView):
             "notify::active-playlist", self._on_active_playlist_changed)
 
         # Selection is only possible from the context menu
-        self._window.disconnect(self._selection_mode_id)
+        self.disconnect(self._selection_mode_id)
 
         self.show_all()
 

gnomemusic/views/songsview.py
@@ -155,9 +155,7 @@ class SongsView(BaseView):
 
     @log
     def _on_selection_mode_changed(self, widget, data=None):
-        selection_mode = self._window.props.selection_mode
-        if (selection_mode == self.props.selection_mode
-                or self.get_parent().get_visible_child() != self):
+        if self.get_parent().get_visible_child() != self:
             return
 
         super()._on_selection_mode_changed(widget, data)