57ee33fe996072cbc79fa8e3912bfb4289df6ef3
Jean Felder <jfelder@src.gnome.org>
2020-01-22 18:06:24 +0100

    grltrackerwrapper: Retrieve album artist for songs

--- gnomemusic/grilowrappers/grltrackerwrapper.py
+++ gnomemusic/grilowrappers/grltrackerwrapper.py
@@ -306,6 +306,7 @@ class GrlTrackerWrapper(GObject.GObject):
             nie:title(?song) AS ?title
             nmm:artistName(nmm:performer(?song)) AS ?artist
             nie:title(nmm:musicAlbum(?song)) AS ?album
+            ?album_artist AS ?album_artist
             nfo:duration(?song) AS ?duration
             nie:usageCounter(?song) AS ?play_count
             nmm:trackNumber(?song) AS ?track_number
@@ -324,6 +325,10 @@ class GrlTrackerWrapper(GObject.GObject):
                 ?song tracker:hasExternalReference ?track_id .
                 ?track_id tracker:referenceSource
                     "https://musicbrainz.org/doc/Track" .
+            }
+            OPTIONAL {
+                ?song nmm:musicAlbum ?album .
+                ?album nmm:albumArtist/nmm:artistName ?album_artist .
             }
             OPTIONAL {
                 ?song nao:hasTag ?tag .
@@ -398,6 +403,7 @@ class GrlTrackerWrapper(GObject.GObject):
             nie:title(?song) AS ?title
             nmm:artistName(nmm:performer(?song)) AS ?artist
             nie:title(nmm:musicAlbum(?song)) AS ?album
+            ?album_artist AS ?album_artist
             nfo:duration(?song) AS ?duration
             nie:usageCounter(?song) AS ?play_count
             nmm:trackNumber(?song) AS ?track_number
@@ -416,6 +422,10 @@ class GrlTrackerWrapper(GObject.GObject):
                 ?song tracker:hasExternalReference ?track_id .
                 ?track_id tracker:referenceSource
                     "https://musicbrainz.org/doc/Track" .
+            }
+            OPTIONAL {
+                ?song nmm:musicAlbum ?album .
+                ?album nmm:albumArtist/nmm:artistName ?album_artist .
             }
             OPTIONAL {
                 ?song nao:hasTag ?tag .
@@ -663,6 +673,7 @@ class GrlTrackerWrapper(GObject.GObject):
             nie:title(?song) AS ?title
             nmm:artistName(nmm:performer(?song)) AS ?artist
             nie:title(nmm:musicAlbum(?song)) AS ?album
+            ?album_artist AS ?album_artist
             nfo:duration(?song) AS ?duration
             nmm:trackNumber(?song) AS ?track_number
             nmm:setNumber(nmm:musicAlbumDisc(?song)) AS ?album_disc_number
@@ -683,6 +694,7 @@ class GrlTrackerWrapper(GObject.GObject):
                 ?track_id tracker:referenceSource
                     "https://musicbrainz.org/doc/Track" .
             }
+            OPTIONAL { ?album nmm:albumArtist/nmm:artistName ?album_artist . }
             OPTIONAL { ?song nao:hasTag ?tag .
                        FILTER (?tag = nao:predefined-tag-favorite) } .
             FILTER ( tracker:id(?album) = %(album_id)s
