81eb34b576dc4f3e1cc2df81feee5bec3179d1f9
Sumaid Syed <sumaidsyed@gmail.com>
2020-01-23 14:18:55 +0100

    selectiontoolbar: Add support to launch song editor dialog
    This dialog can be displayed if only one song is selected.
    
    Based on an initial patch by Jean Felder.

--- gnomemusic/window.py
+++ gnomemusic/window.py
@@ -43,6 +43,7 @@ from gnomemusic.widgets.playertoolbar import PlayerToolbar
 from gnomemusic.widgets.playlistdialog import PlaylistDialog
 from gnomemusic.widgets.searchheaderbar import SearchHeaderBar
 from gnomemusic.widgets.selectiontoolbar import SelectionToolbar  # noqa: F401
+from gnomemusic.widgets.songeditordialog import SongEditorDialog
 from gnomemusic.windowplacement import WindowPlacement
 
 
@@ -125,6 +126,8 @@ class Window(Gtk.ApplicationWindow):
         self._player_toolbar = PlayerToolbar()
         self._player_toolbar.props.player = self._player
 
+        self._selection_toolbar.props.stack = self._stack
+
         self._headerbar.connect(
             'back-button-clicked', self._switch_back_from_childview)
 
@@ -171,6 +174,7 @@ class Window(Gtk.ApplicationWindow):
 
         self._selection_toolbar.connect(
             'add-to-playlist', self._on_add_to_playlist)
+        self._selection_toolbar.connect("edit-details", self._on_edit_tags)
         self._search.connect("notify::state", self._on_search_state_changed)
 
         self._headerbar.props.state = HeaderBar.State.MAIN
@@ -478,6 +482,22 @@ class Window(Gtk.ApplicationWindow):
 
         self.props.selection_mode = False
         playlist_dialog.destroy()
+
+    def _on_edit_tags(self, widget):
+        if self._stack.get_visible_child() == self.views[View.PLAYLIST]:
+            return
+
+        selected_songs = self._app.props.coreselection.props.selected_items
+
+        if len(selected_songs) < 1:
+            return
+
+        grilo = self._app.props.coremodel.props.grilo
+        song_editor_dialog = SongEditorDialog(self, selected_songs[0], grilo)
+        song_editor_dialog.run()
+
+        self.props.selection_mode = False
+        song_editor_dialog.destroy()
 
     def set_player_visible(self, visible):
         """Set PlayWidget action visibility
