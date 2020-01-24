b61160bf2755d9fbf74706b71d3495af09f09172
Jean Felder <jfelder@src.gnome.org>
2020-01-24 02:47:44 +0100

    grltrackerwrapper: Retrieve musicbrainz release group id for songs

--- gnomemusic/grilowrappers/grltrackerwrapper.py
+++ gnomemusic/grilowrappers/grltrackerwrapper.py
@@ -52,6 +52,7 @@ class GrlTrackerWrapper(GObject.GObject):
         Grl.METADATA_KEY_MB_ARTIST_ID,
         Grl.METADATA_KEY_MB_RECORDING_ID,
         Grl.METADATA_KEY_MB_RELEASE_ID,
+        Grl.METADATA_KEY_MB_RELEASE_GROUP_ID,
         Grl.METADATA_KEY_MB_TRACK_ID,
         Grl.METADATA_KEY_PLAY_COUNT,
         Grl.METADATA_KEY_THUMBNAIL,
@@ -330,6 +331,8 @@ class GrlTrackerWrapper(GObject.GObject):
             tracker:referenceIdentifier(?artist_id) AS ?mb_artist_id
             nie:title(nmm:musicAlbum(?song)) AS ?album
             tracker:referenceIdentifier(?release_id) AS ?mb_release_id
+            tracker:referenceIdentifier(?release_group_id)
+                AS ?mb_release_group_id
             ?album_artist AS ?album_artist
             nfo:duration(?song) AS ?duration
             nie:usageCounter(?song) AS ?play_count
@@ -359,6 +362,12 @@ class GrlTrackerWrapper(GObject.GObject):
                 ?album tracker:hasExternalReference ?release_id .
                 ?release_id tracker:referenceSource
                     "https://musicbrainz.org/doc/Release" .
+            }
+            OPTIONAL {
+                ?song nmm:musicAlbum ?album .
+                ?album tracker:hasExternalReference ?release_group_id .
+                ?release_group_id tracker:referenceSource
+                    "https://musicbrainz.org/doc/Release_Group" .
             }
             OPTIONAL {
                 ?song nmm:performer ?artist .
@@ -443,6 +452,8 @@ class GrlTrackerWrapper(GObject.GObject):
             tracker:referenceIdentifier(?artist_id) AS ?mb_artist_id
             nie:title(nmm:musicAlbum(?song)) AS ?album
             tracker:referenceIdentifier(?release_id) AS ?mb_release_id
+            tracker:referenceIdentifier(?release_group_id)
+                AS ?mb_release_group_id
             ?album_artist AS ?album_artist
             nfo:duration(?song) AS ?duration
             nie:usageCounter(?song) AS ?play_count
@@ -472,6 +483,12 @@ class GrlTrackerWrapper(GObject.GObject):
                 ?album tracker:hasExternalReference ?release_id .
                 ?release_id tracker:referenceSource
                     "https://musicbrainz.org/doc/Release" .
+            }
+            OPTIONAL {
+                ?song nmm:musicAlbum ?album .
+                ?album tracker:hasExternalReference ?release_group_id .
+                ?release_group_id tracker:referenceSource
+                    "https://musicbrainz.org/doc/Release_Group" .
             }
             OPTIONAL {
                 ?song nmm:performer ?artist .
@@ -728,6 +745,8 @@ class GrlTrackerWrapper(GObject.GObject):
             tracker:referenceIdentifier(?artist_id) AS ?mb_artist_id
             nie:title(nmm:musicAlbum(?song)) AS ?album
             tracker:referenceIdentifier(?release_id) AS ?mb_release_id
+            tracker:referenceIdentifier(?release_group_id)
+                AS ?mb_release_group_id
             ?album_artist AS ?album_artist
             nfo:duration(?song) AS ?duration
             nmm:trackNumber(?song) AS ?track_number
@@ -754,6 +773,11 @@ class GrlTrackerWrapper(GObject.GObject):
                 ?album tracker:hasExternalReference ?release_id .
                 ?release_id tracker:referenceSource
                     "https://musicbrainz.org/doc/Release" .
+            }
+            OPTIONAL {
+                ?album tracker:hasExternalReference ?release_group_id .
+                ?release_group_id tracker:referenceSource
+                    "https://musicbrainz.org/doc/Release_Group" .
             }
             OPTIONAL {
                 ?song nmm:performer ?artist .
