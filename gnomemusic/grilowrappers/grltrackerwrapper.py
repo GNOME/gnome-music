609ebb68e861031ccf2ee0ba336da2cac16ef125
Jean Felder <jfelder@src.gnome.org>
2020-01-24 02:11:14 +0100

    grltrackerwrapper: Retrieve musicbrainz track id for songs

--- gnomemusic/grilowrappers/grltrackerwrapper.py
+++ gnomemusic/grilowrappers/grltrackerwrapper.py
@@ -51,6 +51,7 @@ class GrlTrackerWrapper(GObject.GObject):
         Grl.METADATA_KEY_FAVOURITE,
         Grl.METADATA_KEY_ID,
         Grl.METADATA_KEY_MB_RECORDING_ID,
+        Grl.METADATA_KEY_MB_TRACK_ID,
         Grl.METADATA_KEY_PLAY_COUNT,
         Grl.METADATA_KEY_THUMBNAIL,
         Grl.METADATA_KEY_TITLE,
@@ -299,6 +300,7 @@ class GrlTrackerWrapper(GObject.GObject):
             nie:title(?song) AS ?title
             tracker:id(?song) AS ?id
             tracker:referenceIdentifier(?recording_id) AS ?mb_recording_id
+            tracker:referenceIdentifier(?track_id) AS ?mb_track_id
             ?song
             nie:url(?song) AS ?url
             nie:title(?song) AS ?title
@@ -317,6 +319,9 @@ class GrlTrackerWrapper(GObject.GObject):
                 ?song tracker:hasExternalReference ?recording_id .
                 ?recording_id tracker:referenceSource
                     "https://musicbrainz.org/doc/Recording" .
+                ?song tracker:hasExternalReference ?track_id .
+                ?track_id tracker:referenceSource
+                    "https://musicbrainz.org/doc/Track" .
                 ?song nao:hasTag ?tag .
                 FILTER (?tag = nao:predefined-tag-favorite)
             }
@@ -383,6 +388,7 @@ class GrlTrackerWrapper(GObject.GObject):
             nie:title(?song) AS ?title
             tracker:id(?song) AS ?id
             tracker:referenceIdentifier(?recording_id) AS ?mb_recording_id
+            tracker:referenceIdentifier(?track_id) AS ?mb_track_id
             ?song
             nie:url(?song) AS ?url
             nie:title(?song) AS ?title
@@ -401,6 +407,9 @@ class GrlTrackerWrapper(GObject.GObject):
                 ?song tracker:hasExternalReference ?recording_id .
                 ?recording_id tracker:referenceSource
                     "https://musicbrainz.org/doc/Recording" .
+                ?song tracker:hasExternalReference ?track_id .
+                ?track_id tracker:referenceSource
+                    "https://musicbrainz.org/doc/Track" .
                 ?song nao:hasTag ?tag .
                 FILTER (?tag = nao:predefined-tag-favorite)
             }
@@ -641,6 +650,7 @@ class GrlTrackerWrapper(GObject.GObject):
             ?song AS ?tracker_urn
             tracker:id(?song) AS ?id
             tracker:referenceIdentifier(?recording_id) AS ?mb_recording_id
+            tracker:referenceIdentifier(?track_id) AS ?mb_track_id
             nie:url(?song) AS ?url
             nie:title(?song) AS ?title
             nmm:artistName(nmm:performer(?song)) AS ?artist
@@ -659,6 +669,9 @@ class GrlTrackerWrapper(GObject.GObject):
                 ?song tracker:hasExternalReference ?recording_id .
                 ?recording_id tracker:referenceSource
                     "https://musicbrainz.org/doc/Recording" .
+                ?song tracker:hasExternalReference ?track_id .
+                ?track_id tracker:referenceSource
+                    "https://musicbrainz.org/doc/Track" .
                 ?song nao:hasTag ?tag .
                 FILTER (?tag = nao:predefined-tag-favorite)
             }
