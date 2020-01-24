9fff4cd63e662b325969f489fcf1e7e39a42a754
Jean Felder <jfelder@src.gnome.org>
2020-01-24 02:38:39 +0100

    grltrackerwrapper: Retrieve musicbrainz artist id for songs

--- gnomemusic/grilowrappers/grltrackerwrapper.py
+++ gnomemusic/grilowrappers/grltrackerwrapper.py
@@ -49,6 +49,7 @@ class GrlTrackerWrapper(GObject.GObject):
         Grl.METADATA_KEY_DURATION,
         Grl.METADATA_KEY_FAVOURITE,
         Grl.METADATA_KEY_ID,
+        Grl.METADATA_KEY_MB_ARTIST_ID,
         Grl.METADATA_KEY_MB_RECORDING_ID,
         Grl.METADATA_KEY_MB_TRACK_ID,
         Grl.METADATA_KEY_PLAY_COUNT,
@@ -308,6 +309,7 @@ class GrlTrackerWrapper(GObject.GObject):
             nie:url(?song) AS ?url
             nie:title(?song) AS ?title
             nmm:artistName(nmm:performer(?song)) AS ?artist
+            tracker:referenceIdentifier(?artist_id) AS ?mb_artist_id
             nie:title(nmm:musicAlbum(?song)) AS ?album
             ?album_artist AS ?album_artist
             nfo:duration(?song) AS ?duration
@@ -332,6 +334,12 @@ class GrlTrackerWrapper(GObject.GObject):
             OPTIONAL {
                 ?song nmm:musicAlbum ?album .
                 ?album nmm:albumArtist/nmm:artistName ?album_artist .
+            }
+            OPTIONAL {
+                ?song nmm:performer ?artist .
+                ?artist tracker:hasExternalReference ?artist_id .
+                ?artist_id tracker:referenceSource
+                    "https://musicbrainz.org/doc/Artist" .
             }
             OPTIONAL {
                 ?song nao:hasTag ?tag .
@@ -407,6 +415,7 @@ class GrlTrackerWrapper(GObject.GObject):
             nie:url(?song) AS ?url
             nie:title(?song) AS ?title
             nmm:artistName(nmm:performer(?song)) AS ?artist
+            tracker:referenceIdentifier(?artist_id) AS ?mb_artist_id
             nie:title(nmm:musicAlbum(?song)) AS ?album
             ?album_artist AS ?album_artist
             nfo:duration(?song) AS ?duration
@@ -431,6 +440,12 @@ class GrlTrackerWrapper(GObject.GObject):
             OPTIONAL {
                 ?song nmm:musicAlbum ?album .
                 ?album nmm:albumArtist/nmm:artistName ?album_artist .
+            }
+            OPTIONAL {
+                ?song nmm:performer ?artist .
+                ?artist tracker:hasExternalReference ?artist_id .
+                ?artist_id tracker:referenceSource
+                    "https://musicbrainz.org/doc/Artist" .
             }
             OPTIONAL {
                 ?song nao:hasTag ?tag .
@@ -677,6 +692,7 @@ class GrlTrackerWrapper(GObject.GObject):
             nie:url(?song) AS ?url
             nie:title(?song) AS ?title
             nmm:artistName(nmm:performer(?song)) AS ?artist
+            tracker:referenceIdentifier(?artist_id) AS ?mb_artist_id
             nie:title(nmm:musicAlbum(?song)) AS ?album
             ?album_artist AS ?album_artist
             nfo:duration(?song) AS ?duration
@@ -700,6 +716,12 @@ class GrlTrackerWrapper(GObject.GObject):
                     "https://musicbrainz.org/doc/Track" .
             }
             OPTIONAL { ?album nmm:albumArtist/nmm:artistName ?album_artist . }
+            OPTIONAL {
+                ?song nmm:performer ?artist .
+                ?artist tracker:hasExternalReference ?artist_id .
+                ?artist_id tracker:referenceSource
+                    "https://musicbrainz.org/doc/Artist" .
+            }
             OPTIONAL { ?song nao:hasTag ?tag .
                        FILTER (?tag = nao:predefined-tag-favorite) } .
             FILTER ( tracker:id(?album) = %(album_id)s
