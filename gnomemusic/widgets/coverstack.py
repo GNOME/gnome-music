68169fbb287fcfba5d71c64e58606b1095075607
Erik Inkinen <erik.inkinen@gmail.com>
2020-03-15 11:13:27 +0200

    Stop using show_all Currently, the application uses show_all function, but it will not be supported in GTK4. To fix the situation, replace show_all with the visibilty property. #320

--- gnomemusic/widgets/coverstack.py
+++ gnomemusic/widgets/coverstack.py
@@ -69,7 +69,11 @@ class CoverStack(Gtk.Stack):
 
         self.connect("destroy", self._on_destroy)
 
-        self.show_all()
+        self._loading_cover.props.visible = True
+        self._cover_a.props.visible = True
+        self._cover_b.props.visible = True
+
+        self.props.visible = True
 
     @GObject.Property(type=object, flags=GObject.ParamFlags.READWRITE)
     def size(self):
