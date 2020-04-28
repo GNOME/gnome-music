1697fe1c1a2e8c1dc8ffd7935ed968cff64a9cd1
Marinus Schraal <mschraal@gnome.org>
2020-04-29 00:52:58 +0200

    IconTheme gtk4 fixes *incomplete*
    should use get_for_display

--- gnomemusic/albumartcache.py
+++ gnomemusic/albumartcache.py
@@ -123,7 +123,7 @@ class DefaultIcon(GObject.GObject):
         MUSIC = 'folder-music-symbolic'
 
     _cache = {}
-    _default_theme = Gtk.IconTheme.get_default()
+    _default_theme = Gtk.IconTheme.new()
 
     def __init__(self):
         super().__init__()
