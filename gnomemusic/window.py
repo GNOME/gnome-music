7e7a5ea7b6b9f0c134e7643d090c1f92bfd9cb40
Marinus Schraal <mschraal@gnome.org>
2020-05-03 01:12:46 +0200

    Correctly reveal PlayerToolbar

--- gnomemusic/window.py
+++ gnomemusic/window.py
@@ -444,7 +444,7 @@ class Window(Gtk.ApplicationWindow):
     def _on_selection_mode_changed(self, widget, data=None):
         if (not self.props.selection_mode
                 and self._player.state == Playback.STOPPED):
-            self._player_toolbar.hide()
+            self._player_toolbar.props.revealed = False
 
     def _on_add_to_playlist(self, widget):
         if self.props.active_view == self.views[View.PLAYLIST]:
@@ -469,4 +469,4 @@ class Window(Gtk.ApplicationWindow):
 
         :param bool visible: actionbar visibility
         """
-        self._player_toolbar.set_visible(visible)
+        self._player_toolbar.props.revealed = visible
