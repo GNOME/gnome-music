7fcd909e0d81791ab5e855688edd75641f6be7d2
Jean Felder <jfelder@src.gnome.org>
2020-05-16 21:32:38 +0200

    discbox: Simplify discbox initialization
    There is no need for the "set_disc_number" method: a CoreDisc number
    cannot change.
    
    By using the CoreDisc in the constructor instead of its model, it is
    possible to directly set the model and the disc label.

--- gnomemusic/widgets/artistalbumwidget.py
+++ gnomemusic/widgets/artistalbumwidget.py
@@ -93,7 +93,7 @@ class ArtistAlbumWidget(Gtk.Box):
         corealbum.props.model.items_changed(0, 0, 0)
 
     def _create_widget(self, disc):
-        disc_box = self._create_disc_box(disc.props.disc_nr, disc.model)
+        disc_box = self._create_disc_box(disc)
 
         self._disc_list_box.bind_property(
             "selection-mode", disc_box, "selection-mode",
@@ -102,9 +102,8 @@ class ArtistAlbumWidget(Gtk.Box):
 
         return disc_box
 
-    def _create_disc_box(self, disc_nr, album_model):
-        disc_box = DiscBox(album_model)
-        disc_box.set_disc_number(disc_nr)
+    def _create_disc_box(self, coredisc):
+        disc_box = DiscBox(coredisc)
         disc_box.props.show_durations = True
         disc_box.props.show_favorites = True
         disc_box.props.show_song_numbers = True
