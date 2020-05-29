9aee322d79628ff5924802fbe28d9cc825b19af4
Jean Felder <jfelder@src.gnome.org>
2020-05-29 21:46:14 +0200

    gstplayer: Disable gapless for non local files

--- gnomemusic/gstplayer.py
+++ gnomemusic/gstplayer.py
@@ -93,7 +93,8 @@ class GstPlayer(GObject.GObject):
 
         self._player.connect("about-to-finish", self._on_about_to_finish)
 
-        self._state = Playback.STOPPED
+        self.props.state = Playback.STOPPED
+        self._gapless_enabled = False
 
     def _setup_replaygain(self):
         """Set up replaygain"""
@@ -126,7 +127,8 @@ class GstPlayer(GObject.GObject):
             self._player.set_property("audio-filter", None)
 
     def _on_about_to_finish(self, klass):
-        self.emit("about-to-finish")
+        if self._gapless_enabled:
+            self.emit("about-to-finish")
 
     def _on_async_done(self, bus, message):
         if self._seek:
@@ -258,6 +260,7 @@ class GstPlayer(GObject.GObject):
 
         :param string url: url to load
         """
+        self._gapless_enabled = (url_.startswith("file"))
         self._player.set_property('uri', url_)
 
     @GObject.Property
@@ -294,6 +297,16 @@ class GstPlayer(GObject.GObject):
         For internal use only.
         """
         self._duration = duration
+
+    @GObject.Property(
+        type=bool, default=False, flags=GObject.ParamFlags.READABLE)
+    def gapless_enabled(self):
+        """Gapless state
+
+        :return: True if gapless is enabled.
+        :rtype: bool
+        """
+        return self._gapless_enabled
 
     def seek(self, seconds):
         """Seek to position
