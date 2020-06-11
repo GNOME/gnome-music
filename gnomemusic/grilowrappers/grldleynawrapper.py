79af556832a9812968d8226337df6e838eba11aa
Chinmay Gurjar <chnmgurjar@gmail.com>
2020-06-11 21:43:05 +0530

    grldleynawrapper: Implements initial fill of artists from dleyna source
    Fetches artists from the dleyna source and appends it to the artist model.

--- gnomemusic/grilowrappers/grldleynawrapper.py
+++ gnomemusic/grilowrappers/grldleynawrapper.py
@@ -3,6 +3,7 @@ gi.require_versions({"Grl": "0.3"})
 from gi.repository import Grl, GObject
 
 from gnomemusic.corealbum import CoreAlbum
+from gnomemusic.coreartist import CoreArtist
 from gnomemusic.coredisc import CoreDisc
 from gnomemusic.coresong import CoreSong
 
@@ -36,6 +37,8 @@ class GrlDleynaWrapper(GObject.GObject):
         self._source = source
         self._albums_model = self._coremodel.props.albums
         self._album_ids = {}
+        self._artists_model = self._coremodel.props.artists
+        self._artist_ids = {}
         self._hash = {}
         self._window = application.props.window
 
@@ -47,6 +50,7 @@ class GrlDleynaWrapper(GObject.GObject):
 
         self._initial_songs_fill(self.props.source)
         self._initial_albums_fill(self.props.source)
+        self._initial_artists_fill(self.props.source)
 
     @GObject.Property(type=Grl.Source, default=None)
     def source(self):
@@ -131,6 +135,37 @@ class GrlDleynaWrapper(GObject.GObject):
 
         source.query(query, self.METADATA_KEYS, options, _add_to_albums_model)
 
+    def _initial_artists_fill(self, source):
+        self._window.notifications_popup.push_loading()
+        artists_added = []
+
+        def _add_to_artists_model(source, op_id, media, remaining, error):
+            if error:
+                self._log.warning("Error: {}".format(error))
+                self._window.notifications_popup.pop_loading()
+                return
+
+            artist = CoreArtist(self._application, media)
+            self._artist_ids[media.get_id()] = artist
+            artists_added.append(artist)
+            if len(artists_added) == self._SPLICE_SIZE:
+                self._artists_model.splice(
+                    self._artists_model.get_n_items(), 0, artists_added)
+                artists_added.clear()
+
+            if remaining == 0:
+                self._artists_model.splice(
+                    self._artists_model.get_n_items(), 0, artists_added)
+                self._window.notifications_popup.pop_loading()
+
+        query = """upnp:class = 'object.container.person.musicArtist'
+        """.replace("\n", " ").strip()
+
+        options = self._fast_options.copy()
+
+        source.query(
+            query, self.METADATA_KEYS, options, _add_to_artists_model)
+
     def get_album_discs(self, media, disc_model):
         disc_nr = 1
         coredisc = CoreDisc(self._application, media, disc_nr)
@@ -148,6 +183,46 @@ class GrlDleynaWrapper(GObject.GObject):
         options = self._fast_options.copy()
 
         self.props.source.query(query, self.METADATA_KEYS, options, callback)
+
+    def get_artist_albums(self, artist, model):
+        self._window.notifications_popup.push_loading()
+        artist_name = artist.get_title()
+
+        query = """
+            upnp:artist = '%(artist_name)s'
+            derviedfrom (upnp:class = 'object.container.person.musicArtist')
+        """.replace("\n", " ").strip() % {
+            "artist_name": artist_name
+        }
+
+        albums = []
+
+        def query_cb(source, op_id, media, remaining, error):
+            if error:
+                self._log.warning("Error: {}".format(error))
+                self._window.notifications_popup.pop_loading()
+                return
+
+            if media.get_album() not in albums:
+                albums.append(media.get_album())
+
+            if remaining == 0:
+                model.set_filter_func(albums_filter, albums)
+                self._window.notifications_popup.pop_loading()
+                return
+
+        def albums_filter(corealbum, albums):
+            for media in albums:
+                if (media == corealbum.props.title
+                        and corealbum.props.source ==
+                            self._source.props.source_id):
+                    return True
+
+            return False
+
+        options = self._fast_options.copy()
+        self.props.source.query(
+            query, self.METADATA_KEYS, options, query_cb)
 
     def search(self, text):
         pass
