5309a16bfa04a2e1586c97d991770ef0076b48a6
Chinmay Gurjar <chnmgurjar@gmail.com>
2020-07-01 15:23:48 +0530

    grldleynawrapper: Hard force media to load sequentially
    Songs are used to populate album discs and albums discs are used to
    populate artist albums. As all the calls are made async, there can be a
    possiblility that album disc query get executed before song query
    or artist albums query execute before the album disc query.
    
    This problem can be avoided by making sure the media are loaded
    sequentially, first songs, then albums and at last artists.

--- gnomemusic/grilowrappers/grldleynawrapper.py
+++ gnomemusic/grilowrappers/grldleynawrapper.py
@@ -49,8 +49,6 @@ class GrlDleynaWrapper(GObject.GObject):
         self.props.source = source
 
         self._initial_songs_fill()
-        self._initial_albums_fill()
-        self._initial_artists_fill()
 
     @GObject.Property(type=Grl.Source, default=None)
     def source(self):
@@ -80,6 +78,7 @@ class GrlDleynaWrapper(GObject.GObject):
                 self._songs_model.splice(
                     self._songs_model.get_n_items(), 0, songs_added)
                 self._window.notifications_popup.pop_loading()
+                self._initial_albums_fill()
 
                 return
 
@@ -129,6 +128,9 @@ class GrlDleynaWrapper(GObject.GObject):
                 self._albums_model.splice(
                     self._albums_model.get_n_items(), 0, albums_added)
                 self._window.notifications_popup.pop_loading()
+                self._initial_artists_fill()
+
+                return
 
         query = """upnp:class = 'object.container.album.musicAlbum'
         """.replace("\n", " ").strip()
