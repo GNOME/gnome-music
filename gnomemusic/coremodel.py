GNOME/gnome-music

commit 9b89bdac292e9d0bdbba31d4fe93059426e49d7a

coremodel: Add active_core_object property to set a player playlist
Only some widgets and the MPRIS objet can set the playlist player by
calling the set_player_model() function from the CoreModel. Once the
player playlist is loaded, the appropriate song is loaded and the
player starts playing.
Each type of player playlist can be set by only one widget. However, a
playlist type can also set by the MPRIS object. When PlaylistsWidget
or MPRIS set a new player playlist, the active_playlist property is
used to inform the other object of the change.

This commit generalizes this approach by introducing a new
active_core_object property. A set_player_model call is replaced by
setting the active coreobject. Based on the type of media,
_set_player_model() is called with the correct parameters.

The next commit will remove the active_playlist property which is
redundant with the active_core_object property.

gnomemusic/coremodel.py
@@ -107,6 +107,7 @@ class CoreModel(GObject.GObject):
 
         self._playlist_model = Gio.ListStore.new(CoreSong)
         self._playlist_model_sort = Gfm.SortListModel.new(self._playlist_model)
+        self._active_core_object = None
 
         self._songs_search_proxy = Gio.ListStore.new(Gfm.FilterListModel)
         self._songs_search_flatten = Gfm.FlattenListModel.new(CoreSong)
@@ -140,6 +141,8 @@ class CoreModel(GObject.GObject):
             self._user_playlists_model_filter)
         self._user_playlists_model_sort.set_sort_func(
             utils.wrap_list_store_sort_func(self._playlists_sort))
+
+        self._search = application.props.search
 
         self._songs_model.connect(
             "items-changed", self._on_songs_items_changed)
@@ -184,7 +187,7 @@ class CoreModel(GObject.GObject):
             playlist_a.props.creation_date)
         return math.copysign(1, date_diff)
 
-    def set_player_model(self, playlist_type, model):
+    def _set_player_model(self, playlist_type, model):
         """Set the model for PlayerPlaylist to use
 
         This fills playlist model based on the playlist type and model
@@ -298,6 +301,45 @@ class CoreModel(GObject.GObject):
         self._previous_playlist_model = model
 
         self.emit("playlist-loaded", playlist_type)
+
+    @GObject.Property(default=None)
+    def active_core_object(self):
+        """Get the current playing core object
+        (album, artist, playlist, search result or song).
+
+        :returns: current media
+        :rtype: CoreObject
+        """
+        return self._active_core_object
+
+    @active_core_object.setter
+    def active_core_object(self, value):
+        """Set the current playing core object
+        (album, artist, playlist, search result or song).
+
+        :param CoreObject value: new core object to play
+        """
+        self._active_core_object = value
+        if isinstance(value, CoreAlbum):
+            playlist_type = PlayerPlaylist.Type.ALBUM
+            model = value.props.model
+        elif isinstance(value, CoreArtist):
+            playlist_type = PlayerPlaylist.Type.ARTIST
+            model = value.props.model
+        elif isinstance(value, Playlist):
+            playlist_type = PlayerPlaylist.Type.PLAYLIST
+            model = value.props.model
+        # If the search is active, it means that the search view is visible,
+        # so the player playlist is a list of songs from the search result.
+        # Otherwise, it's a list of songs from the songs view.
+        elif self._search.props.search_mode_active:
+            playlist_type = PlayerPlaylist.Type.SEARCH_RESULT
+            model = self._song_search_flatten
+        else:
+            playlist_type = PlayerPlaylist.Type.SONGS
+            model = self._songs_model
+
+        self._set_player_model(playlist_type, model)
 
     @GObject.Property(
         type=Gio.ListStore, default=None, flags=GObject.ParamFlags.READABLE)

gnomemusic/mpris.py
@@ -737,8 +737,7 @@ class MPRIS(DBusInterface):
         loaded_id = self._coremodel.connect(
             "playlist-loaded", _on_playlist_loaded)
         self._coremodel.props.active_playlist = playlist
-        self._coremodel.set_player_model(
-            PlayerPlaylist.Type.PLAYLIST, playlist.props.model)
+        self._coremodel.props.active_core_object = playlist
 
     def _activate_playlist(self, playlist_path):
         """Starts playing the given playlist (MPRIS Method).

gnomemusic/views/searchview.py
@@ -27,7 +27,6 @@ from gettext import gettext as _
 
 from gi.repository import Gdk, GObject, Gtk
 
-from gnomemusic.player import PlayerPlaylist
 from gnomemusic.search import Search
 from gnomemusic.widgets.albumcover import AlbumCover
 from gnomemusic.widgets.albumwidget import AlbumWidget
@@ -249,8 +248,7 @@ class SearchView(Gtk.Stack):
                 and not self.props.selection_mode):
             # self.emit('song-activated', widget)
 
-            self._coremodel.set_player_model(
-                PlayerPlaylist.Type.SEARCH_RESULT, self._model)
+            self._coremodel.props.active_core_object = widget.props.coresong
             self._player.play(widget.props.coresong)
 
         # FIXME: Need to ignore the event from the checkbox.

gnomemusic/views/songsview.py
@@ -26,7 +26,6 @@ from gettext import gettext as _
 from gi.repository import Gdk, GObject, Gtk, Pango
 
 from gnomemusic.coresong import CoreSong
-from gnomemusic.player import PlayerPlaylist
 from gnomemusic.utils import SongStateIcon
 from gnomemusic.widgets.starhandlerwidget import StarHandlerWidget
 
@@ -151,8 +150,7 @@ class SongsView(Gtk.ScrolledWindow):
 
         itr = self._model.get_iter(path)
         coresong = self._model[itr][7]
-        self._coremodel.set_player_model(
-            PlayerPlaylist.Type.SONGS, self._model)
+        self._coremodel.props.active_core_object = coresong
 
         self._player.play(coresong)
 

gnomemusic/widgets/albumwidget.py
@@ -26,7 +26,6 @@ from gettext import ngettext
 
 from gi.repository import GObject, Grl, Gtk
 
-from gnomemusic.player import PlayerPlaylist
 from gnomemusic.utils import ArtSize
 from gnomemusic.widgets.disclistboxwidget import DiscBox
 from gnomemusic.widgets.disclistboxwidget import DiscListBox  # noqa: F401
@@ -167,8 +166,7 @@ class AlbumWidget(Gtk.EventBox):
 
         signal_id = self._coremodel.connect(
             "playlist-loaded", _on_playlist_loaded)
-        self._coremodel.set_player_model(
-            PlayerPlaylist.Type.ALBUM, self._album_model)
+        self._coremodel.props.active_core_object = self._corealbum
 
         return True
 

gnomemusic/widgets/artistalbumswidget.py
@@ -24,7 +24,6 @@
 
 from gi.repository import GObject, Gtk
 
-from gnomemusic.player import PlayerPlaylist
 from gnomemusic.widgets.artistalbumwidget import ArtistAlbumWidget
 
 
@@ -74,7 +73,7 @@ class ArtistAlbumsWidget(Gtk.ListBox):
             coremodel.disconnect(signal_id)
 
         signal_id = coremodel.connect("playlist-loaded", _on_playlist_loaded)
-        coremodel.set_player_model(PlayerPlaylist.Type.ARTIST, self._model)
+        coremodel.props.active_core_object = self._artist
 
     def _add_album(self, corealbum):
         row = Gtk.ListBoxRow()

gnomemusic/widgets/playlistswidget.py
@@ -24,7 +24,6 @@
 
 from gi.repository import Gdk, GObject, Gtk
 
-from gnomemusic.player import PlayerPlaylist
 from gnomemusic.widgets.notificationspopup import PlaylistNotification
 from gnomemusic.widgets.playlistcontextmenu import PlaylistContextMenu
 from gnomemusic.widgets.playlistcontrols import PlaylistControls  # noqa: F401
@@ -119,8 +118,7 @@ class PlaylistsWidget(Gtk.Box):
         signal_id = self._coremodel.connect(
             "playlist-loaded", _on_playlist_loaded)
         self._coremodel.props.active_playlist = current_playlist
-        self._coremodel.set_player_model(
-            PlayerPlaylist.Type.PLAYLIST, current_playlist.props.model)
+        self._coremodel.props.active_core_object = current_playlist
 
     def _on_song_widget_moved(self, target, source_position):
         target_position = target.get_parent().get_index()

commit f1273198bf1bb21634964ff1140cdc6e757e52e9

coremodel: Correct variable name
Commit 0f0e69ba0e renamed some variables, commit 9b89bdac29
accidentally reintroduced one of the old names. This resulted in a crash
while trying to play songs from a search.

gnomemusic/coremodel.py
@@ -333,7 +333,7 @@ class CoreModel(GObject.GObject):
         # Otherwise, it's a list of songs from the songs view.
         elif self._search.props.search_mode_active:
             playlist_type = PlayerPlaylist.Type.SEARCH_RESULT
-            model = self._song_search_flatten
+            model = self._songs_search_flatten
         else:
             playlist_type = PlayerPlaylist.Type.SONGS
             model = self._songs_model