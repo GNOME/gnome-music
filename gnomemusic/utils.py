55095d56217c8629d3e38f9a0b029cf5224f1e52
Jean Felder <jfelder@src.gnome.org>
2020-11-20 20:00:15 +0100

    utils: Remove unused xlarge in artsize

--- gnomemusic/utils.py
+++ gnomemusic/utils.py
@@ -39,7 +39,6 @@ class ArtSize(Enum):
     SMALL = (48, 48)
     MEDIUM = (128, 128)
     LARGE = (256, 256)
-    XLARGE = (512, 512)
 
     def __init__(self, width, height):
         """Intialize width and height"""
