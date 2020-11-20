d456e33bb34bc8c5ddcfc79e9e6e42b0aaef12e7
Jean Felder <jfelder@src.gnome.org>
2020-11-20 20:01:07 +0100

    utils: Remove unused small in artsize

--- gnomemusic/utils.py
+++ gnomemusic/utils.py
@@ -36,7 +36,6 @@ from gnomemusic.musiclogger import MusicLogger
 class ArtSize(Enum):
     """Enum for icon sizes"""
     XSMALL = (34, 34)
-    SMALL = (48, 48)
     MEDIUM = (128, 128)
     LARGE = (256, 256)
 
