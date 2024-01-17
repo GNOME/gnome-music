e949364991228f84b6c91476c636de0579416868
Marinus Schraal <mschraal@gnome.org>
2024-01-17 15:26:30 +0100

    albumsview: Drop selection-mode

--- gnomemusic/views/albumsview.py
+++ gnomemusic/views/albumsview.py
@@ -48,7 +48,6 @@ class AlbumsView(Adw.Bin):
     icon_name = GObject.Property(
         type=str, default="media-optical-cd-audio-symbolic",
         flags=GObject.ParamFlags.READABLE)
-    selection_mode = GObject.Property(type=bool, default=False)
     title = GObject.Property(
         type=str, default=_("Albums"), flags=GObject.ParamFlags.READABLE)
 
@@ -79,34 +78,11 @@ class AlbumsView(Adw.Bin):
 
         self._gridview.props.factory = list_item_factory
 
-        self._selection_model = Gtk.MultiSelection.new(
+        selection_model = Gtk.MultiSelection.new(
             self._application.props.coremodel.props.albums_sort)
-        self._gridview.props.model = self._selection_model
+        self._gridview.props.model = selection_model
 
         self._gridview.connect("activate", self._on_album_activated)
-
-        self.bind_property(
-            "selection-mode", self._gridview, "single-click-activate",
-            GObject.BindingFlags.SYNC_CREATE
-            | GObject.BindingFlags.INVERT_BOOLEAN)
-        self.bind_property(
-            "selection-mode", self._gridview, "enable-rubberband",
-            GObject.BindingFlags.SYNC_CREATE)
-        self.bind_property(
-            "selection-mode", self._window, "selection-mode",
-            GObject.BindingFlags.DEFAULT)
-
-        self._window.connect(
-            "notify::selection-mode", self._on_selection_mode_changed)
-
-    def _on_selection_mode_changed(self, widget, data=None):
-        selection_mode = self._window.props.selection_mode
-        if selection_mode == self.props.selection_mode:
-            return
-
-        self.props.selection_mode = selection_mode
-        if not self.props.selection_mode:
-            self.deselect_all()
 
     def _on_album_activated(self, widget, position):
         corealbum = widget.props.model[position]
@@ -114,40 +90,17 @@ class AlbumsView(Adw.Bin):
         album_page = AlbumNavigationPage(self._application, corealbum)
         self._navigation_view.push(album_page)
 
-    def _toggle_all_selection(self, selected):
-        """Selects or deselects all items.
-        """
-        with self._application.props.coreselection.freeze_notify():
-            if selected:
-                self._selection_model.select_all()
-            else:
-                self._selection_model.unselect_all()
-
-    def select_all(self):
-        self._toggle_all_selection(True)
-
-    def deselect_all(self):
-        self._toggle_all_selection(False)
-
     def _setup_list_item(
             self, factory: Gtk.SignalListItemFactory,
             list_item: Gtk.ListItem) -> None:
         builder = Gtk.Builder.new_from_resource(
             "/org/gnome/Music/ui/AlbumCoverListItem.ui")
         list_item.props.child = builder.get_object("_album_cover")
-        cover_image = list_item.props.child.get_first_child().get_first_child()
+        cover_image = list_item.props.child.get_first_child()
         cover_image.set_size_request(
             ArtSize.MEDIUM.width, ArtSize.MEDIUM.height)
         cover_image.props.paintable = CoverPaintable(
             self, ArtSize.MEDIUM, icon_type=DefaultIconType.ALBUM)
-
-        self.bind_property(
-            "selection-mode", list_item, "selectable",
-            GObject.BindingFlags.SYNC_CREATE)
-        self.bind_property(
-            "selection-mode", list_item, "activatable",
-            GObject.BindingFlags.SYNC_CREATE
-            | GObject.BindingFlags.INVERT_BOOLEAN)
 
     def _bind_list_item(
             self, factory: Gtk.SignalListItemFactory,
@@ -155,8 +108,7 @@ class AlbumsView(Adw.Bin):
         album_cover = list_item.props.child
         corealbum = list_item.props.item
 
-        cover_image = album_cover.get_first_child().get_first_child()
-        check = cover_image.get_next_sibling()
+        cover_image = album_cover.get_first_child()
         album_label = album_cover.get_first_child().get_next_sibling()
         artist_label = album_label.get_next_sibling()
 
@@ -168,45 +120,10 @@ class AlbumsView(Adw.Bin):
         b3 = corealbum.bind_property(
             "artist", artist_label, "label", GObject.BindingFlags.SYNC_CREATE)
 
-        b4 = list_item.bind_property(
-            "selected", corealbum, "selected",
-            GObject.BindingFlags.SYNC_CREATE)
-        b5 = list_item.bind_property(
-            "selected", check, "active", GObject.BindingFlags.SYNC_CREATE)
-        b6 = self.bind_property(
-            "selection-mode", check, "visible",
-            GObject.BindingFlags.SYNC_CREATE)
-
-        def on_activated(widget, value):
-            if check.props.active:
-                self._selection_model.select_item(
-                    list_item.get_position(), False)
-            else:
-                self._selection_model.unselect_item(
-                    list_item.get_position())
-
-        # the listitem selected property is read-only.
-        # It cannot be bound from the check active property.
-        # It is necessary to update the selection model in order
-        # to update it.
-        check.connect("notify::active", on_activated)
-
-        self._list_item_bindings[list_item] = [b1, b2, b3, b4, b5, b6]
+        self._list_item_bindings[list_item] = [b1, b2, b3]
 
     def _unbind_list_item(
             self, factory: Gtk.SignalListItemFactory,
             list_item: Gtk.ListItem) -> None:
         bindings = self._list_item_bindings.pop(list_item)
         [binding.unbind() for binding in bindings]
-
-        album_cover = list_item.props.child
-
-        cover_image = album_cover.get_first_child().get_first_child()
-        check = cover_image.get_next_sibling()
-
-        signal_id, detail_id = GObject.signal_parse_name(
-            "notify::active", check, True)
-        handler_id = GObject.signal_handler_find(
-            check, GObject.SignalMatchType.ID, signal_id, detail_id, None, 0,
-            0)
-        check.disconnect(handler_id)
